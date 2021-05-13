cc82c52018513ab9f975f9d274be74f93e6fca1d
Butygin <ivan.butygin@intel.com>
2021-05-13 14:34:23 +0300

    Get rid of numba copypasted code 1

--- numba_dpcomp/mlir/lowering.py
+++ numba_dpcomp/mlir/lowering.py
@@ -36,72 +36,23 @@ from .math_runtime import *
 
 class mlir_lower(orig_Lower):
     def lower(self):
-        # Emit the Env into the module
-        self.emit_environment_object()
-        if self.generator_info is None:
+        if USE_MLIR:
+            self.emit_environment_object()
             self.genlower = None
             self.lower_normal_function(self.fndesc)
+            self.context.post_lowering(self.module, self.library)
         else:
-            self.genlower = self.GeneratorLower(self)
-            self.gentype = self.genlower.gentype
-
-            self.genlower.lower_init_func(self)
-            self.genlower.lower_next_func(self)
-            if self.gentype.has_finalizer:
-                self.genlower.lower_finalize_func(self)
-
-        if config.DUMP_LLVM:
-            print(("LLVM DUMP %s" % self.fndesc).center(80, '-'))
-            if config.HIGHLIGHT_DUMPS:
-                try:
-                    from pygments import highlight
-                    from pygments.lexers import LlvmLexer as lexer
-                    from pygments.formatters import Terminal256Formatter
-                    from numba.misc.dump_style import by_colorscheme
-                    print(highlight(self.module.__repr__(), lexer(),
-                                    Terminal256Formatter(
-                                        style=by_colorscheme())))
-                except ImportError:
-                    msg = "Please install pygments to see highlighted dumps"
-                    raise ValueError(msg)
-            else:
-                print(self.module)
-            print('=' * 80)
-
-        # Special optimization to remove NRT on functions that do not need it.
-        if self.context.enable_nrt and self.generator_info is None:
-            removerefctpass.remove_unnecessary_nrt_usage(self.function,
-                                                         context=self.context,
-                                                         fndesc=self.fndesc)
-
-        # Run target specific post lowering transformation
-        self.context.post_lowering(self.module, self.library)
-
-        if not USE_MLIR:
-            # Materialize LLVM Module
-            self.library.add_ir_module(self.module)
+            orig_Lower.lower(self)
 
     def lower_normal_function(self, fndesc):
-        """
-        Lower non-generator *fndesc*.
-        """
         if USE_MLIR:
             mod_ir = self.mlir_blob
             import llvmlite.binding as llvm
             mod = llvm.parse_bitcode(mod_ir)
-
-        self.setup_function(fndesc)
-
-        if USE_MLIR:
+            self.setup_function(fndesc)
             self.library.add_llvm_module(mod);
         else:
-            # Init argument values
-            self.extract_function_arguments()
-            entry_block_tail = self.lower_function_body()
-
-            # Close tail of entry block
-            self.builder.position_at_end(entry_block_tail)
-            self.builder.branch(self.blkmap[self.firstblk])
+            orig_Lower.lower_normal_function(self, desc)
 
 
 @register_pass(mutates_CFG=True, analysis_only=False)
