72618a56ebed5dad51c2e12fad93e52fd6b80c04
Alexander-Makaryev <alexander.makaryev@gmail.com>
2021-06-10 06:04:32 -0500

    refactor numpy tests

--- numba_dpcomp/mlir/tests/test_numpy.py
+++ numba_dpcomp/mlir/tests/test_numpy.py
@@ -19,6 +19,7 @@ import numpy as np
 from numba.tests.support import TestCase
 import unittest
 import itertools
+import pytest
 
 def _vectorize_reference(func, arg1):
     ret = np.empty(arg1.shape, arg1.dtype)
@@ -31,6 +32,29 @@ _arr_1d_float = [1.0,2.1,3.2,4.3,5.4,6.5,7.6,8.7]
 _arr_2d_int = [[1,2,3,4],[5,6,7,8]]
 _arr_2d_float = [[1.0,2.1,3.2,4.3],[5.4,6.5,7.6,8.7]]
 _test_arrays = [_arr_1d_int, _arr_1d_float, _arr_2d_int, _arr_2d_float]
+
+@pytest.mark.parametrize("py_func",
+                         [lambda a: a.sum(),
+                          lambda a: np.sum(a),
+                          lambda a: np.sqrt(a),
+                          lambda a: np.square(a),
+                          lambda a: np.log(a),
+                          lambda a: np.sin(a),
+                          lambda a: np.cos(a),
+                          lambda a: a.size,
+                          # lambda a: a.T, TODO: need fortran layout support
+                          lambda a: a.T.T,
+                         ],
+                         ids=["a.sum", "sum", "sqrt", "square", "log", "sin",
+                              "cos", "a.size", "a.T.T"])
+@pytest.mark.parametrize("arr_list",
+                         _test_arrays,
+                         ids=["1d_int", "1d_float", "2d_int", "2d_float"])
+def test_unary(py_func, arr_list):
+    jit_func = njit(py_func)
+    arr = np.array(arr_list)
+    assert_equal(py_func(arr), jit_func(arr))
+
 class TestMlirBasic(TestCase):
 
     def test_staticgetitem(self):
@@ -57,26 +81,6 @@ class TestMlirBasic(TestCase):
         jit_func = njit(py_func)
         arr = np.asarray([5,6,7])
         assert_equal(py_func(arr), jit_func(arr))
-
-    def test_unary(self):
-        funcs = [
-            lambda a: a.sum(),
-            lambda a: np.sum(a),
-            lambda a: np.sqrt(a),
-            lambda a: np.square(a),
-            lambda a: np.log(a),
-            lambda a: np.sin(a),
-            lambda a: np.cos(a),
-            lambda a: a.size,
-            # lambda a: a.T, TODO: need fortran layout support
-            lambda a: a.T.T,
-        ]
-
-        for py_func in funcs:
-            jit_func = njit(py_func)
-            for a in _test_arrays:
-                arr = np.array(a)
-                assert_equal(py_func(arr), jit_func(arr))
 
     def test_binary(self):
         funcs = [
