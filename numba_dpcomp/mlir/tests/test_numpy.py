b5585a15b635fd21b8f4428d3741c2e7b461ae14
Butygin <ivan.butygin@intel.com>
2021-06-23 17:00:51 +0300

    refactor some more tests

--- numba_dpcomp/mlir/tests/test_numpy.py
+++ numba_dpcomp/mlir/tests/test_numpy.py
@@ -164,47 +164,47 @@ def test_getitem3():
     arr = np.asarray([[[1,2,3],[5,6,7]]])
     assert_equal(py_func(arr, 0, 0), jit_func(arr, 0, 0))
 
+def test_array_len():
+    def py_func(a):
+        return len(a)
+
+    jit_func = njit(py_func)
+    arr = np.asarray([5,6,7])
+    assert_equal(py_func(arr), jit_func(arr))
+
+@parametrize_function_variants("py_func", [
+    'lambda a: np.sum(a, axis=0)',
+    'lambda a: np.sum(a, axis=1)',
+    ])
+@pytest.mark.parametrize("arr", [
+    np.array([[1,2,3],[4,5,6]], dtype=np.int32),
+    np.array([[1,2,3],[4,5,6]], dtype=np.float32),
+    ])
+def test_sum_axis(py_func, arr):
+    jit_func = njit(py_func)
+    assert_equal(py_func(arr), jit_func(arr))
+
+def test_sum_add():
+    def py_func(a, b):
+        return np.add(a, b).sum()
+
+    jit_func = njit(py_func)
+    arr1 = np.asarray([1,2,3])
+    arr2 = np.asarray([4,5,6])
+    assert_equal(py_func(arr1, arr2), jit_func(arr1, arr2))
+
+def test_sum_add2():
+    def py_func(a, b, c):
+        t = np.add(a, b)
+        return np.add(t, c).sum()
+
+    jit_func = njit(py_func)
+    arr1 = np.asarray([1,2,3])
+    arr2 = np.asarray([4,5,6])
+    arr3 = np.asarray([7,8,9])
+    assert_equal(py_func(arr1, arr2, arr3), jit_func(arr1, arr2, arr3))
+
 class TestMlirBasic(TestCase):
-    def test_array_len(self):
-        def py_func(a):
-            return len(a)
-
-        jit_func = njit(py_func)
-        arr = np.asarray([5,6,7])
-        assert_equal(py_func(arr), jit_func(arr))
-
-    def test_sum_axis(self):
-        funcs = [
-            lambda a: np.sum(a, axis=0),
-            lambda a: np.sum(a, axis=1),
-        ]
-
-        for py_func in funcs:
-            jit_func = njit(py_func)
-            arr = np.array([[1,2,3],[4,5,6]])
-            for a in [arr, arr.astype(np.float32)]:
-                assert_equal(py_func(a), jit_func(a))
-
-    def test_sum_add(self):
-        def py_func(a, b):
-            return np.add(a, b).sum()
-
-        jit_func = njit(py_func)
-        arr1 = np.asarray([1,2,3])
-        arr2 = np.asarray([4,5,6])
-        assert_equal(py_func(arr1, arr2), jit_func(arr1, arr2))
-
-    def test_sum_add2(self):
-        def py_func(a, b, c):
-            t = np.add(a, b)
-            return np.add(t, c).sum()
-
-        jit_func = njit(py_func)
-        arr1 = np.asarray([1,2,3])
-        arr2 = np.asarray([4,5,6])
-        arr3 = np.asarray([7,8,9])
-        assert_equal(py_func(arr1, arr2, arr3), jit_func(arr1, arr2, arr3))
-
     def test_dot(self):
         def py_func(a, b):
             return np.dot(a, b)
