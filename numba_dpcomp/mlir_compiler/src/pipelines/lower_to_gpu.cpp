9c4c1b3d8a491e79d0fdb87fdce2971d5baaa1f8
Ivan Butygin <ivan.butygin@intel.com>
2021-07-22 20:44:49 +0300

    Preprocess strided memrefs access for spirv

--- numba_dpcomp/mlir_compiler/src/pipelines/lower_to_gpu.cpp
+++ numba_dpcomp/mlir_compiler/src/pipelines/lower_to_gpu.cpp
@@ -14,19 +14,24 @@
 
 #include "pipelines/lower_to_gpu.hpp"
 
+#include <mlir/Conversion/AffineToStandard/AffineToStandard.h>
 #include <mlir/Conversion/GPUToSPIRV/GPUToSPIRVPass.h>
 #include <mlir/Conversion/SCFToGPU/SCFToGPUPass.h>
+#include <mlir/Dialect/Affine/IR/AffineOps.h>
 #include <mlir/Dialect/GPU/ParallelLoopMapper.h>
 #include <mlir/Dialect/GPU/Passes.h>
+#include <mlir/Dialect/MemRef/IR/MemRef.h>
 #include <mlir/Dialect/SCF/SCF.h>
 #include <mlir/Dialect/SPIRV/IR/TargetAndABI.h>
 #include <mlir/Pass/PassManager.h>
+#include <mlir/Transforms/GreedyPatternRewriteDriver.h>
 #include <mlir/Transforms/Passes.h>
 
 #include "base_pipeline.hpp"
 #include "pipelines/lower_to_llvm.hpp"
 
 #include "plier/compiler/pipeline_registry.hpp"
+#include "plier/dialect.hpp"
 
 namespace {
 struct ParallelLoopGPUMappingPass
@@ -38,6 +43,126 @@ struct ParallelLoopGPUMappingPass
 
   void runOnFunction() override {
     mlir::greedilyMapParallelSCFToGPU(getFunction().getBody());
+  }
+};
+
+static void setInsertionPointToStart(mlir::OpBuilder &builder,
+                                     mlir::Value val) {
+  if (auto parentOp = val.getDefiningOp()) {
+    builder.setInsertionPointAfter(parentOp);
+  } else {
+    builder.setInsertionPointToStart(val.getParentBlock());
+  }
+}
+
+static mlir::Value getFlatIndex(mlir::OpBuilder &builder, mlir::Location loc,
+                                mlir::Value memref, mlir::ValueRange indices) {
+  auto memrefType = memref.getType().cast<mlir::MemRefType>();
+  auto rank = static_cast<unsigned>(memrefType.getRank());
+  assert(memrefType.getAffineMaps().size() <= 1);
+  auto affineMap =
+      memrefType.getAffineMaps().empty()
+          ? mlir::AffineMap::getMultiDimIdentityMap(rank, builder.getContext())
+          : memrefType.getAffineMaps()[0];
+  assert(indices.size() == rank);
+  llvm::SmallVector<mlir::Value> applyOperands(rank * 2 + 1);
+  {
+    mlir::OpBuilder::InsertionGuard g(builder);
+    setInsertionPointToStart(builder, memref);
+    llvm::copy(indices, applyOperands.begin());
+    applyOperands[rank] =
+        builder.createOrFold<plier::ExtractMemrefMetadataOp>(loc, memref);
+    auto strides =
+        llvm::MutableArrayRef<mlir::Value>(applyOperands).drop_front(rank + 1);
+    for (auto i : llvm::seq(0u, rank)) {
+      strides[i] =
+          builder.createOrFold<plier::ExtractMemrefMetadataOp>(loc, memref, i);
+    }
+  }
+  return builder.createOrFold<mlir::AffineApplyOp>(loc, affineMap,
+                                                   applyOperands);
+}
+
+static mlir::Value getFlatMemref(mlir::OpBuilder &builder, mlir::Location loc,
+                                 mlir::Value memref) {
+  auto memrefType = memref.getType().cast<mlir::MemRefType>();
+  auto resultType = mlir::MemRefType::get(mlir::ShapedType::kDynamicSize,
+                                          memrefType.getElementType());
+  mlir::OpBuilder::InsertionGuard g(builder);
+  setInsertionPointToStart(builder, memref);
+  mlir::OpFoldResult offset = builder.getIndexAttr(0);
+  mlir::OpFoldResult size =
+      builder.createOrFold<plier::UndefOp>(loc, builder.getIndexType());
+  mlir::OpFoldResult stride = builder.getIndexAttr(1);
+  return builder.createOrFold<mlir::memref::ReinterpretCastOp>(
+      loc, resultType, memref, offset, size, stride);
+}
+
+static bool needFlatten(mlir::Value val) {
+  auto type = val.getType().cast<mlir::MemRefType>();
+  return !type.getAffineMaps().empty() ||
+         (type.getRank() > 1 && !type.hasStaticShape());
+}
+
+struct FlattenLoad : public mlir::OpRewritePattern<mlir::memref::LoadOp> {
+  using OpRewritePattern::OpRewritePattern;
+
+  mlir::LogicalResult
+  matchAndRewrite(mlir::memref::LoadOp op,
+                  mlir::PatternRewriter &rewriter) const override {
+    if (!op->getParentOfType<mlir::gpu::LaunchOp>())
+      return mlir::failure();
+
+    auto memref = op.memref();
+    if (!needFlatten(memref))
+      return mlir::failure();
+
+    auto loc = op.getLoc();
+    auto flatIndex = getFlatIndex(rewriter, loc, memref, op.indices());
+    auto flatMemref = getFlatMemref(rewriter, loc, memref);
+    rewriter.replaceOpWithNewOp<mlir::memref::LoadOp>(op, flatMemref,
+                                                      flatIndex);
+    return mlir::success();
+  }
+};
+
+struct FlattenStore : public mlir::OpRewritePattern<mlir::memref::StoreOp> {
+  using OpRewritePattern::OpRewritePattern;
+
+  mlir::LogicalResult
+  matchAndRewrite(mlir::memref::StoreOp op,
+                  mlir::PatternRewriter &rewriter) const override {
+    if (!op->getParentOfType<mlir::gpu::LaunchOp>())
+      return mlir::failure();
+
+    auto memref = op.memref();
+    if (!needFlatten(memref))
+      return mlir::failure();
+
+    auto loc = op.getLoc();
+    auto flatIndex = getFlatIndex(rewriter, loc, memref, op.indices());
+    auto flatMemref = getFlatMemref(rewriter, loc, memref);
+    rewriter.replaceOpWithNewOp<mlir::memref::StoreOp>(op, op.value(),
+                                                       flatMemref, flatIndex);
+    return mlir::success();
+  }
+};
+
+struct UnstrideMemrefsPass
+    : public mlir::PassWrapper<UnstrideMemrefsPass, mlir::FunctionPass> {
+  virtual void
+  getDependentDialects(mlir::DialectRegistry &registry) const override {
+    registry.insert<mlir::memref::MemRefDialect>();
+    registry.insert<mlir::gpu::GPUDialect>();
+  }
+
+  void runOnFunction() override {
+    mlir::OwningRewritePatternList patterns(&getContext());
+
+    patterns.insert<FlattenLoad, FlattenStore>(&getContext());
+
+    (void)mlir::applyPatternsAndFoldGreedily(getFunction(),
+                                             std::move(patterns));
   }
 };
 
@@ -65,14 +190,38 @@ struct AbiAttrsPass
   }
 };
 
+struct SetSPIRVCapabilitiesPass
+    : public mlir::PassWrapper<SetSPIRVCapabilitiesPass,
+                               mlir::OperationPass<mlir::ModuleOp>> {
+  void runOnOperation() override {
+    namespace spirv = mlir::spirv;
+    auto context = &getContext();
+    auto caps = {spirv::Capability::Shader};
+    auto exts = {spirv::Extension::SPV_KHR_storage_buffer_storage_class};
+    auto triple =
+        spirv::VerCapExtAttr::get(spirv::Version::V_1_0, caps, exts, context);
+    auto attr = spirv::TargetEnvAttr::get(
+        triple, spirv::Vendor::Unknown, spirv::DeviceType::Unknown,
+        spirv::TargetEnvAttr::kUnknownDeviceID,
+        spirv::getDefaultResourceLimits(context));
+    auto module = getOperation();
+    module->setAttr(spirv::getTargetEnvAttrName(), attr);
+  }
+};
+
 static void populateLowerToGPUPipeline(mlir::OpPassManager &pm) {
   pm.addNestedPass<mlir::FuncOp>(
       std::make_unique<ParallelLoopGPUMappingPass>());
   pm.addNestedPass<mlir::FuncOp>(mlir::createParallelLoopToGpuPass());
   pm.addNestedPass<mlir::FuncOp>(mlir::createCanonicalizerPass());
+  pm.addNestedPass<mlir::FuncOp>(std::make_unique<UnstrideMemrefsPass>());
+  pm.addNestedPass<mlir::FuncOp>(mlir::createLowerAffinePass());
+  pm.addNestedPass<mlir::FuncOp>(mlir::createCanonicalizerPass());
+  pm.addPass(mlir::createCSEPass());
   pm.addPass(mlir::createGpuKernelOutliningPass());
   pm.addPass(mlir::createCanonicalizerPass());
   pm.addNestedPass<mlir::gpu::GPUModuleOp>(std::make_unique<AbiAttrsPass>());
+  pm.addPass(std::make_unique<SetSPIRVCapabilitiesPass>());
   pm.addPass(mlir::createConvertGPUToSPIRVPass());
   pm.addPass(mlir::createCanonicalizerPass());
 }
