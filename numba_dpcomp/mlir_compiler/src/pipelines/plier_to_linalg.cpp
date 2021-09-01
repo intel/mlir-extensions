34ff5bf5296d36ebf0502672c7c790b841f454d1
geexie <marina.kolpakova@intel.com>
2021-09-01 15:57:34 +0300

    steps and reductions

--- numba_dpcomp/mlir_compiler/src/pipelines/plier_to_linalg.cpp
+++ numba_dpcomp/mlir_compiler/src/pipelines/plier_to_linalg.cpp
@@ -1663,7 +1663,7 @@ public:
 
   LogicalResult matchAndRewrite(scf::ParallelOp op,
                                 PatternRewriter &rewriter) const override {
-    // Temporary disable if contains induction variables
+    // Temporary disable if contains induction variables, it's not clear for now what is to do with those inductions
     if (op.initVals().size() != 0)
       return rewriter.notifyMatchFailure(op, "scf.parallel constains indcution variables");
 
@@ -1671,6 +1671,31 @@ public:
     if (op.step().size() != 1)
       return rewriter.notifyMatchFailure(op, "scf.parallel ND range");
 
+    // Check if steps are constants
+    SmallVector<int64_t> newSteps;
+    for (auto s : op.step()) {
+      if (auto c = s.getDefiningOp<ConstantIndexOp>()) {
+        newSteps.push_back(c.getValue());
+      } else {
+        return rewriter.notifyMatchFailure(op, "scf.parallel->affine.parallel non constant step");
+      }
+    }
+
+    // Awoid conversing reductions for now
+    if (llvm::any_of(op.region().getOps(), [&](const Operation& each) {
+        return !!isa<scf::ReduceOp>(each); })) {
+      return rewriter.notifyMatchFailure(op, "scf.parallel->affine.parallel reduction is detected");
+    }
+
+    // just for the case if we reductions
+    SmallVector<LoopReduction> reductions; // fill them from found scf.reduce op
+    auto reducedValueTypes = llvm::to_vector<4>(llvm::map_range(
+        reductions, [](const LoopReduction &red) { return red.value.getType(); }));
+
+    auto reductionKinds = llvm::to_vector<4>(llvm::map_range(
+        reductions, [](const LoopReduction &red) { return red.kind; }));
+
+    llvm::errs() << "debuging scf->affine pass\n";
     op.dump();
 
 
@@ -1681,47 +1706,22 @@ public:
 				       ) ;
 
     outer_loop.dump();
-    // auto loop = rewriter.create<mlir::AffineParallelOp>(op.getLoc(), op.lowerBound().getTypes(), op.lowerBound(),
-                                                                    //  op.upperBound().getTypes(), op.upperBound());
 
-    // loop.dump();
-
-    // op.step().front().dump();
-    // op.lowerBound().dump();
-    // op.upperBound().dump();
-    // op.getBody()->dump();
-
-
-    SmallVector<LoopReduction> parallelReductions;
-    // Fail early if there are iter arguments that are not reductions.
-    // unsigned numReductions = parallelReductions.size();
-    // if (numReductions != op.initVals() forOp.getNumIterOperands())
-    //   return failure();
-
-    // Location loc = forOp.getLoc();
-    // OpBuilder outsideBuilder(op);
     AffineMap lowerBoundMap = AffineMap::getConstantMap(0, op.getContext());//forOp.getLowerBoundMap();
     ValueRange lowerBoundOperands = {};//forOp.getLowerBoundOperands();
     AffineMap upperBoundMap = AffineMap::getConstantMap(42, op.getContext());//forOp.getUpperBoundMap();
     ValueRange upperBoundOperands = {};//forOp.getUpperBoundOperands();
 
-    ArrayRef<int64_t> steps;
-
-    // // Creating empty 1-D affine.parallel op.
-    auto reducedValues = llvm::to_vector<4>(llvm::map_range(
-        parallelReductions, [](const LoopReduction &red) { return red.value; }));
-
-    auto reductionKinds = llvm::to_vector<4>(llvm::map_range(
-        parallelReductions, [](const LoopReduction &red) { return red.kind; }));
-
+    // Creating empty 1-D affine.parallel op.
     AffineParallelOp newPloop = rewriter.create<AffineParallelOp>(
-        op.getLoc(), ValueRange(reducedValues).getTypes(), reductionKinds,
+        op.getLoc(), reducedValueTypes, reductionKinds,
         llvm::makeArrayRef(lowerBoundMap), lowerBoundOperands,
-        llvm::makeArrayRef(upperBoundMap), upperBoundOperands,steps
-        // llvm::makeArrayRef(op.step())
+        llvm::makeArrayRef(upperBoundMap), upperBoundOperands,
+        llvm::makeArrayRef(newSteps)
         );
 
     newPloop.dump();
+
     // // Steal the body of the old affine for op.
     // newPloop.region().takeBody(op.region());
     // Operation *yieldOp = &newPloop.getBody()->back();
