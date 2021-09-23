24f4ba7fbec62e1d3bc96769350000698b574175
Ivan Butygin <ivan.butygin@intel.com>
2021-09-23 21:39:26 +0300

    WhileOp to ForOp lowering refactoring (#9)

--- dpcomp/include/plier/transforms/loop_utils.hpp
+++ dpcomp/include/plier/transforms/loop_utils.hpp
@@ -15,6 +15,7 @@
 #pragma once
 
 #include <llvm/ADT/STLExtras.h>
+#include <llvm/ADT/SmallVector.h>
 
 namespace mlir {
 struct LogicalResult;
@@ -26,7 +27,8 @@ class Type;
 class Region;
 namespace scf {
 class ForOp;
-}
+class WhileOp;
+} // namespace scf
 } // namespace mlir
 
 namespace plier {
@@ -34,6 +36,15 @@ class GetiterOp;
 }
 
 namespace plier {
+bool canLowerWhileToFor(mlir::scf::WhileOp whileOp);
+llvm::SmallVector<mlir::scf::ForOp, 2> lowerWhileToFor(
+    mlir::scf::WhileOp whileOp, mlir::PatternRewriter &builder,
+    llvm::function_ref<std::tuple<mlir::Value, mlir::Value, mlir::Value>(
+        mlir::OpBuilder &, mlir::Location)>
+        getBounds,
+    llvm::function_ref<mlir::Value(mlir::OpBuilder &, mlir::Location,
+                                   mlir::Type, mlir::Value)>
+        getIterVal);
 mlir::LogicalResult lowerWhileToFor(
     plier::GetiterOp getiter, mlir::PatternRewriter &builder,
     llvm::function_ref<std::tuple<mlir::Value, mlir::Value, mlir::Value>(
