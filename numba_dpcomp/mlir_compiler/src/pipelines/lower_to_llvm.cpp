793caf776ece079eb5de461fd336fdcd30960589
Alexander Kalistratov <alexander.kalistratov@intel.com>
2021-10-13 14:38:42 +0300

    Update llvm-sha.txt (#26)

--- numba_dpcomp/mlir_compiler/src/pipelines/lower_to_llvm.cpp
+++ numba_dpcomp/mlir_compiler/src/pipelines/lower_to_llvm.cpp
@@ -40,7 +40,7 @@
 #include <llvm/ADT/Triple.h>
 #include <llvm/ADT/TypeSwitch.h>
 #include <llvm/Support/Host.h>
-#include <llvm/Support/TargetRegistry.h>
+#include <llvm/MC/TargetRegistry.h>
 #include <llvm/Support/TargetSelect.h>
 #include <llvm/Target/TargetMachine.h>
 
