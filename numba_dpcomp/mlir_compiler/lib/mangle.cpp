76896f4a635c9c944ef1d519d57704163b4d2697
Ivan Butygin <ivan.butygin@gmail.com>
2022-01-05 01:58:23 +0300

    Update code style

--- numba_dpcomp/mlir_compiler/lib/mangle.cpp
+++ numba_dpcomp/mlir_compiler/lib/mangle.cpp
@@ -30,7 +30,7 @@ static const constexpr auto PREFIX = "_Z";
 
 template <unsigned Width, mlir::IntegerType::SignednessSemantics Sign,
           char Symbol>
-bool mangle_int(llvm::raw_ostream &res, mlir::Type type) {
+bool mangleInt(llvm::raw_ostream &res, mlir::Type type) {
   if (auto i = type.dyn_cast<mlir::IntegerType>()) {
     if (i.getWidth() == Width && i.getSignedness() == Sign) {
       res << Symbol;
@@ -41,7 +41,7 @@ bool mangle_int(llvm::raw_ostream &res, mlir::Type type) {
 }
 
 template <unsigned Width, char Symbol>
-bool mangle_float(llvm::raw_ostream &res, mlir::Type type) {
+bool mangleFloat(llvm::raw_ostream &res, mlir::Type type) {
   if (auto i = type.dyn_cast<mlir::FloatType>()) {
     if (i.getWidth() == Width) {
       res << Symbol;
@@ -51,81 +51,79 @@ bool mangle_float(llvm::raw_ostream &res, mlir::Type type) {
   return false;
 }
 
-void mangle_memref_impl(llvm::raw_ostream &res, mlir::ShapedType type);
+static void mangleMemrefImpl(llvm::raw_ostream &res, mlir::ShapedType type);
 
-bool mangle_memref(llvm::raw_ostream &res, mlir::Type type) {
+static bool mangleMemref(llvm::raw_ostream &res, mlir::Type type) {
   if (auto m = type.dyn_cast<mlir::ShapedType>()) {
-    mangle_memref_impl(res, m);
+    mangleMemrefImpl(res, m);
     return true;
   }
   return false;
 }
 
-bool mangle_none(llvm::raw_ostream & /*res*/, mlir::Type type) {
-  if (type.isa<mlir::NoneType>()) {
-    // Nothing
-    return true;
-  }
+static bool mangleNone(llvm::raw_ostream & /*res*/, mlir::Type type) {
+  if (type.isa<mlir::NoneType>())
+    return true; // Nothing
+
   return false;
 }
 
 using type_mangler_t = bool (*)(llvm::raw_ostream &, mlir::Type);
 
-static const constexpr type_mangler_t type_manglers[] = {
-    &mangle_int<1, mlir::IntegerType::Signed, 'b'>,
-    &mangle_int<1, mlir::IntegerType::Unsigned, 'b'>,
-    &mangle_int<1, mlir::IntegerType::Signless, 'b'>,
+static const constexpr type_mangler_t typeManglers[] = {
+    &mangleInt<1, mlir::IntegerType::Signed, 'b'>,
+    &mangleInt<1, mlir::IntegerType::Unsigned, 'b'>,
+    &mangleInt<1, mlir::IntegerType::Signless, 'b'>,
 
-    &mangle_int<8, mlir::IntegerType::Signed, 'a'>,
-    &mangle_int<8, mlir::IntegerType::Unsigned, 'h'>,
-    &mangle_int<8, mlir::IntegerType::Signless, 'c'>,
+    &mangleInt<8, mlir::IntegerType::Signed, 'a'>,
+    &mangleInt<8, mlir::IntegerType::Unsigned, 'h'>,
+    &mangleInt<8, mlir::IntegerType::Signless, 'c'>,
 
-    &mangle_int<16, mlir::IntegerType::Signed, 's'>,
-    &mangle_int<16, mlir::IntegerType::Unsigned, 't'>,
-    &mangle_int<16, mlir::IntegerType::Signless, 's'>,
+    &mangleInt<16, mlir::IntegerType::Signed, 's'>,
+    &mangleInt<16, mlir::IntegerType::Unsigned, 't'>,
+    &mangleInt<16, mlir::IntegerType::Signless, 's'>,
 
-    &mangle_int<32, mlir::IntegerType::Signed, 'i'>,
-    &mangle_int<32, mlir::IntegerType::Unsigned, 'j'>,
-    &mangle_int<32, mlir::IntegerType::Signless, 'i'>,
+    &mangleInt<32, mlir::IntegerType::Signed, 'i'>,
+    &mangleInt<32, mlir::IntegerType::Unsigned, 'j'>,
+    &mangleInt<32, mlir::IntegerType::Signless, 'i'>,
 
-    &mangle_int<64, mlir::IntegerType::Signed, 'x'>,
-    &mangle_int<64, mlir::IntegerType::Unsigned, 'm'>,
-    &mangle_int<64, mlir::IntegerType::Signless, 'x'>,
+    &mangleInt<64, mlir::IntegerType::Signed, 'x'>,
+    &mangleInt<64, mlir::IntegerType::Unsigned, 'm'>,
+    &mangleInt<64, mlir::IntegerType::Signless, 'x'>,
 
-    &mangle_int<128, mlir::IntegerType::Signed, 'n'>,
-    &mangle_int<128, mlir::IntegerType::Unsigned, 'o'>,
-    &mangle_int<128, mlir::IntegerType::Signless, 'n'>,
+    &mangleInt<128, mlir::IntegerType::Signed, 'n'>,
+    &mangleInt<128, mlir::IntegerType::Unsigned, 'o'>,
+    &mangleInt<128, mlir::IntegerType::Signless, 'n'>,
 
-    &mangle_float<32, 'f'>,
-    &mangle_float<64, 'd'>,
-    &mangle_float<80, 'e'>,
-    &mangle_float<128, 'g'>,
+    &mangleFloat<32, 'f'>,
+    &mangleFloat<64, 'd'>,
+    &mangleFloat<80, 'e'>,
+    &mangleFloat<128, 'g'>,
 
-    &mangle_memref,
+    &mangleMemref,
 
-    &mangle_none,
+    &mangleNone,
 };
 
-bool check_type(mlir::Type type) {
+static bool checkType(mlir::Type type) {
   llvm::raw_null_ostream ss;
-  for (auto mangler : type_manglers) {
-    if (mangler(ss, type)) {
+  for (auto mangler : typeManglers)
+    if (mangler(ss, type))
       return true;
-    }
-  }
+
   return false;
 }
 
-bool is_valid_char(char c) {
+static bool isValidChar(char c) {
   return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') ||
          (c >= '0' && c <= '9') || (c == '_');
 }
 
-std::string escape_string(llvm::StringRef str) {
+static std::string escapeString(llvm::StringRef str) {
   std::string ret;
   llvm::raw_string_ostream ss(ret);
   for (auto c : str) {
-    if (is_valid_char(c)) {
+    if (isValidChar(c)) {
       ss << c;
     } else {
       ss << "$" << llvm::format_hex_no_prefix(static_cast<unsigned>(c), 2);
@@ -136,14 +134,14 @@ std::string escape_string(llvm::StringRef str) {
 }
 
 template <typename F>
-void mangle_ident_impl(llvm::raw_ostream &res, llvm::StringRef ident,
-                       F &&template_params) {
+static void mangleIdentImpl(llvm::raw_ostream &res, llvm::StringRef ident,
+                            F &&templateParams) {
   assert(!ident.empty());
   llvm::SmallVector<llvm::StringRef> parts;
   ident.split(parts, '.');
   assert(!parts.empty());
-  auto write_part = [&](auto part) {
-    auto escaped = escape_string(part);
+  auto writePart = [&](auto part) {
+    auto escaped = escapeString(part);
     if (std::isdigit(escaped.front())) {
       res << escaped.size() + 1 << '_' << escaped;
     } else {
@@ -151,79 +149,76 @@ void mangle_ident_impl(llvm::raw_ostream &res, llvm::StringRef ident,
     }
   };
   if (parts.size() == 1) {
-    write_part(parts.front());
-    template_params(res);
+    writePart(parts.front());
+    templateParams(res);
   } else {
     res << 'N';
     for (auto &part : parts) {
-      write_part(part);
+      writePart(part);
     }
-    template_params(res);
+    templateParams(res);
     res << 'E';
   }
 }
 
-void mangle_ident(llvm::raw_ostream &res, llvm::StringRef ident) {
+static void mangleIdent(llvm::raw_ostream &res, llvm::StringRef ident) {
   auto dummy = [](auto &) {};
-  mangle_ident_impl(res, ident, dummy);
+  mangleIdentImpl(res, ident, dummy);
 }
 
 template <typename F>
-void mangle_ident(llvm::raw_ostream &res, llvm::StringRef ident,
-                  F &&template_params) {
-  auto wrap_template = [&](llvm::raw_ostream &s) {
+static void mangleIdent(llvm::raw_ostream &res, llvm::StringRef ident,
+                        F &&templateParams) {
+  auto wrapTemplate = [&](llvm::raw_ostream &s) {
     s << 'I';
-    template_params(s);
+    templateParams(s);
     s << 'E';
   };
-  mangle_ident_impl(res, ident, wrap_template);
+  mangleIdentImpl(res, ident, wrapTemplate);
 }
 
-void mangle_type(llvm::raw_ostream &res, mlir::Type type) {
-  for (auto m : type_manglers) {
-    if (m(res, type)) {
+static void mangleType(llvm::raw_ostream &res, mlir::Type type) {
+  for (auto m : typeManglers)
+    if (m(res, type))
       return;
-    }
-  }
+
   llvm_unreachable("Cannot mangle type");
 }
 
-void mangle_memref_impl(llvm::raw_ostream &res, mlir::ShapedType type) {
+static void mangleMemrefImpl(llvm::raw_ostream &res, mlir::ShapedType type) {
   auto params = [&](llvm::raw_ostream &s) {
-    mangle_type(s, type.getElementType());
+    mangleType(s, type.getElementType());
     s << "Li" << type.getRank() << "E";
-    mangle_ident(s, "C");
+    mangleIdent(s, "C");
   };
-  mangle_ident(res, "array", params);
+  mangleIdent(res, "array", params);
 }
 
-void mangle_types(llvm::raw_ostream &res, mlir::TypeRange types) {
-  for (auto type : types) {
-    mangle_type(res, type);
-  }
+static void mangleTypes(llvm::raw_ostream &res, mlir::TypeRange types) {
+  for (auto type : types)
+    mangleType(res, type);
 }
 
 } // namespace
 
 bool mangle(llvm::raw_ostream &res, llvm::StringRef ident,
             mlir::TypeRange types) {
-  for (auto type : types) {
-    if (!check_type(type)) {
+  for (auto type : types)
+    if (!checkType(type))
       return false;
-    }
-  }
+
   res << PREFIX;
-  mangle_ident(res, ident);
-  mangle_types(res, types);
+  mangleIdent(res, ident);
+  mangleTypes(res, types);
   return true;
 }
 
 std::string mangle(llvm::StringRef ident, mlir::TypeRange types) {
   std::string ret;
   llvm::raw_string_ostream ss(ret);
-  if (!mangle(ss, ident, types)) {
+  if (!mangle(ss, ident, types))
     return {};
-  }
+
   ss.flush();
   return ret;
 }
