7996a1d6962ca3ebdedf8b70cfa645c7c477182e
Ivan Butygin <ivan.butygin@gmail.com>
2022-01-30 21:28:07 +0300

    Move common kernel code to kernel_base.py (#127)

---
+++ numba_dpcomp/numba_dpcomp/mlir/kernel_base.py
@@ -0,0 +1,62 @@
+# Copyright 2021 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import copy
+
+def _raise_error(desc):
+    raise ValueError(desc)
+
+def _process_dims(dims):
+    if isinstance(dims, int):
+        return (dims,)
+    elif isinstance(dims, (list, tuple)):
+        n = len(dims)
+        if n > 3:
+            _raise_error(f'Invalid dimentions count: {n}')
+        return tuple(dims)
+    else:
+        _raise_error(f'Invalid dimentions type: {type(dims)}')
+
+class KernelBase:
+    def __init__(self, func):
+        self.global_size = ()
+        self.local_size = ()
+        self.py_func = func
+
+    def copy(self):
+        return copy.copy(self)
+
+    def configure(self, global_size, local_size):
+        global_dim_count = len(global_size)
+        local_dim_count = len(local_size)
+        assert(local_dim_count <= global_dim_count)
+        if local_dim_count != 0 and local_dim_count < global_dim_count:
+            local_size = tuple(local_size[i] if i < local_dim_count else 1 for i in range(global_dim_count))
+        ret = self.copy()
+        ret.global_size = tuple(global_size)
+        ret.local_size = tuple(local_size)
+        return ret
+
+    def check_call_args(self, args, kwargs):
+        if kwargs:
+            _raise_error('kwargs not supported')
+
+    def __getitem__(self, args):
+        nargs = len(args)
+        if nargs < 1 or nargs > 2:
+            _raise_error(f'Invalid kernel arguments count: {nargs}')
+
+        gs = _process_dims(args[0])
+        ls = _process_dims(args[1]) if nargs > 1 else ()
+        return self.configure(gs, ls)
