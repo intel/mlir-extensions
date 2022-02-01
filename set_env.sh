852a18275621f3e10d814b292120a04f4a45d617
Nishant Patel <nishant.b.patel@intel.com>
2022-01-31 16:11:02 -0800

    build and env script

--- set_env.sh
+++ set_env.sh
@@ -3,3 +3,4 @@ chmod -R 777 /localdisk/nbpatel/mlir-llvm
 export LLVM_PATH=/localdisk/nbpatel/mlir-llvm
 export TBB_PATH=/localdisk/nbpatel/mlir-extensions/tbb
 export LEVEL_ZERO_DIR=/localdisk/nbpatel/mlir-extensions/level-zero/level_zero_install
+cd build
