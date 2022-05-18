fae4db0ad87ba40964cbd16ac3a6aeff2cfdd0d6
Sang Ik Lee <sang.ik.lee@intel.com>
2022-05-18 09:24:28 -0700

    Add compile flag to control Numba hotfix.
    Add lowering pattern for plier::Undef
    
    Enable piler::Undef lowering if IMEX_ENABLE_NUMBA_HOTFIX is not defined.

--- mlir/lib/Conversion/gpu_runtime_to_llvm.cpp
+++ mlir/lib/Conversion/gpu_runtime_to_llvm.cpp
@@ -55,6 +55,24 @@ struct LowerTakeContext
   }
 };
 
+#if !defined(IMEX_ENABLE_NUMBA_HOTFIX)
+struct LowerUndef : public mlir::ConvertOpToLLVMPattern<plier::UndefOp> {
+  using ConvertOpToLLVMPattern::ConvertOpToLLVMPattern;
+
+  mlir::LogicalResult
+  matchAndRewrite(plier::UndefOp op, plier::UndefOp::Adaptor /*adaptor*/,
+                  mlir::ConversionPatternRewriter &rewriter) const override {
+    auto converter = getTypeConverter();
+    auto type = converter->convertType(op.getType());
+    if (!type)
+      return mlir::failure();
+
+    rewriter.replaceOpWithNewOp<mlir::LLVM::UndefOp>(op, type);
+    return mlir::success();
+  }
+};
+#endif
+
 struct FunctionCallBuilder {
   FunctionCallBuilder(mlir::StringRef functionName, mlir::Type returnType,
                       mlir::ArrayRef<mlir::Type> argumentTypes)
@@ -849,10 +867,12 @@ struct GPUToLLVMPass
                                                             target);
     mlir::populateGpuToLLVMConversionPatterns(
         converter, patterns, mlir::gpu::getDefaultGpuBinaryAnnotation());
+#if defined(IMEX_ENABLE_NUMBA_HOTFIX)
     mlir::populateFunctionOpInterfaceTypeConversionPattern<mlir::func::FuncOp>(
         patterns, converter);
     mlir::populateReturnOpTypeConversionPattern(patterns, converter);
     mlir::populateCallOpTypeConversionPattern(patterns, converter);
+#endif
 
     target.addDynamicallyLegalOp<mlir::func::FuncOp>(
         [&](mlir::func::FuncOp op) -> llvm::Optional<bool> {
@@ -897,6 +917,9 @@ struct GPUToLLVMPass
         ConvertGpuAllocPattern,
         ConvertGpuDeAllocPattern,
         ConvertGpuSuggestBlockSizePattern,
+#if !defined(IMEX_ENABLE_NUMBA_HOTFIX)
+        LowerUndef,
+#endif
         LowerTakeContext
         // clang-format on
         >(converter);
