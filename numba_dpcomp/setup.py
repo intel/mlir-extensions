2eba7cf66c55bc68d57b8ccc88abc17117bc5a16
Diptorup Deb <diptorup.deb@intel.com>
2022-05-26 18:06:29 -0500

    Initial set of changes to build IMEX separately from numba_dpcomp.

--- numba_dpcomp/setup.py
+++ numba_dpcomp/setup.py
@@ -41,7 +41,7 @@ if int(os.environ.get("DPCOMP_SETUP_RUN_CMAKE", 1)):
     LLVM_PATH = os.environ["LLVM_PATH"]
     LLVM_DIR = os.path.join(LLVM_PATH, "lib", "cmake", "llvm")
     MLIR_DIR = os.path.join(LLVM_PATH, "lib", "cmake", "mlir")
-    TBB_DIR = os.path.join(os.environ["TBB_PATH"], "lib", "cmake", "tbb")
+    # TBB_DIR = os.path.join(os.environ["TBB_PATH"], "lib", "cmake", "tbb")
     CMAKE_INSTALL_PREFIX = root_dir
 
     cmake_build_dir = os.path.join(root_dir, "cmake_build")
@@ -55,15 +55,15 @@ if int(os.environ.get("DPCOMP_SETUP_RUN_CMAKE", 1)):
     NUMPY_INCLUDE_DIR = numpy.get_include()
 
     cmake_cmd += [
-        "..",
+        "../numba_dpcomp",
         "-DCMAKE_BUILD_TYPE=Release",
         "-DLLVM_DIR=" + LLVM_DIR,
         "-DMLIR_DIR=" + MLIR_DIR,
-        "-DTBB_DIR=" + TBB_DIR,
+        # "-DTBB_DIR=" + TBB_DIR,
         "-DCMAKE_INSTALL_PREFIX=" + CMAKE_INSTALL_PREFIX,
         "-DPython3_NumPy_INCLUDE_DIRS=" + NUMPY_INCLUDE_DIR,
         "-DPython3_FIND_STRATEGY=LOCATION",
-        "-DIMEX_ENABLE_NUMBA_FE=ON",
+        # "-DIMEX_ENABLE_NUMBA_FE=ON",
         "-DIMEX_ENABLE_NUMBA_HOTFIX=ON",
         "-DIMEX_ENABLE_TBB_SUPPORT=ON",
     ]
@@ -83,15 +83,15 @@ if int(os.environ.get("DPCOMP_SETUP_RUN_CMAKE", 1)):
     except ImportError:
         print("DPNP not found")
 
-    # GPU/L0
-    LEVEL_ZERO_DIR = os.getenv("LEVEL_ZERO_DIR", None)
-    if LEVEL_ZERO_DIR is None:
-        print("LEVEL_ZERO_DIR is not set")
-    else:
-        print("LEVEL_ZERO_DIR is", LEVEL_ZERO_DIR)
-        cmake_cmd += [
-            "-DIMEX_ENABLE_IGPU_DIALECT=ON",
-        ]
+    # # GPU/L0
+    # LEVEL_ZERO_DIR = os.getenv("LEVEL_ZERO_DIR", None)
+    # if LEVEL_ZERO_DIR is None:
+    #     print("LEVEL_ZERO_DIR is not set")
+    # else:
+    #     print("LEVEL_ZERO_DIR is", LEVEL_ZERO_DIR)
+    #     cmake_cmd += [
+    #         "-DIMEX_ENABLE_IGPU_DIALECT=ON",
+    #     ]
 
     try:
         os.mkdir(cmake_build_dir)
