349ebcdcdc032ad8ed5f8543f3168abece32e524
Frank Schlimbach <frank.schlimbach@intel.com>
2022-06-23 06:33:22 -0500

    adding copyright, file-doc

--- lib/Conversion/IMEXPassDetail.h
+++ lib/Conversion/IMEXPassDetail.h
@@ -1,8 +1,13 @@
 //===- IMEXPassDetail.h - Conversion Pass class details ---------*- C++ -*-===//
 //
+// Copyright 2022 Intel Corporation
 // Part of the IMEX Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file declares IMEX Passes.
 //
 //===----------------------------------------------------------------------===//
 
