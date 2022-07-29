0cee10466e82670b3291e4b1709686d190f27e30
Ivan Butygin <ivan.butygin@gmail.com>
2022-07-30 00:02:37 +0200

    restore IMEX_ENABLE_NUMBA_HOTFIX fix

--- mlir/lib/Conversion/gpu_runtime_to_llvm.cpp
+++ mlir/lib/Conversion/gpu_runtime_to_llvm.cpp
@@ -871,10 +871,12 @@ struct GPUToLLVMPass
                                                             target);
     mlir::populateGpuToLLVMConversionPatterns(
         converter, patterns, mlir::gpu::getDefaultGpuBinaryAnnotation());
+#if defined(IMEX_ENABLE_NUMBA_HOTFIX)
     mlir::populateFunctionOpInterfaceTypeConversionPattern<mlir::func::FuncOp>(
         patterns, converter);
     mlir::populateReturnOpTypeConversionPattern(patterns, converter);
     mlir::populateCallOpTypeConversionPattern(patterns, converter);
+#endif
 
     target.addDynamicallyLegalOp<mlir::func::FuncOp>(
         [&](mlir::func::FuncOp op) -> llvm::Optional<bool> {
