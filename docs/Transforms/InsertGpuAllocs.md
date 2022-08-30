14045606609f0045f4dde1a7b41626d60efde728
Nishant Patel <nishant.b.patel@intel.com>
2022-08-30 10:39:27 -0700

    Add InsertGpuAlloc Pass and doc

---
+++ docs/Transforms/InsertGpuAllocs.md
@@ -0,0 +1,80 @@
+# InsertGpuAllocs Pass
+
+
+The InsertGpuAllocs pass, as the name suggests, inserts the gpu allocs in the IR. Memref alloc is an operation in the memref dialect that can be used to allocate the memory on the host side and or on the device side. The MLIR IR is a mix of host and device code.
+To distinguish between host side memory allocation and device side memory allocation, we convert all the memref.allocs that refer to device (gpu) side memory allocations and references, into gpu.alloc, which is an operation of the upstream GPU dialect. This distinction helps in lowering to llvm and calling the appropriate memory allocation operation at runtime.
+The pass traverses all the memref (load/store) operations inside the gpu launch op in the IR and checks for its aliases and its defining op. If the defining op is a memref.alloc op it replaces that op in the IR with gpu.alloc op, because all the operations under the gpu.launch op are device side computations and will execute on the device. 
+
+# Example
+
+```
+// -----// IR Dump Before {anonymous}::InsertGPUAllocs //----- //
+func.func @main() {
+  %c8 = arith.constant 8 : index
+  %c1 = arith.constant 1 : index
+  %cst = arith.constant 2.200000e+00 : f32
+  %cst_0 = arith.constant 1.100000e+00 : f32
+  %cst_1 = arith.constant 0.000000e+00 : f32
+  %0 = memref.alloc() : memref<8xf32>
+  %1 = memref.alloc() : memref<8xf32>
+  %2 = memref.alloc() : memref<8xf32>
+  %3 = memref.cast %0 : memref<8xf32> to memref<?xf32>
+  %4 = memref.cast %1 : memref<8xf32> to memref<?xf32>
+  %5 = memref.cast %2 : memref<8xf32> to memref<?xf32>
+  call @fillResource1DFloat(%3, %cst_0) : (memref<?xf32>, f32) -> ()
+  call @fillResource1DFloat(%4, %cst) : (memref<?xf32>, f32) -> ()
+  call @fillResource1DFloat(%5, %cst_1) : (memref<?xf32>, f32) -> ()
+  gpu.launch blocks(%arg0, %arg1, %arg2) in (%arg6 = %c8, %arg7 = %c1, %arg8 = %c1) threads(%arg3, %arg4, %arg5) in (%arg9 = %c1, %arg10 = %c1, %arg11 = %c1) {
+    %7 = gpu.block_id  x
+    %8 = memref.load %0[%7] : memref<8xf32>
+    %9 = memref.load %1[%7] : memref<8xf32>
+    %10 = arith.addf %8, %9 : f32
+    memref.store %10, %2[%7] : memref<8xf32>
+    gpu.terminator
+  }
+  %6 = memref.cast %2 : memref<8xf32> to memref<*xf32>
+  call @printMemrefF32(%6) : (memref<*xf32>) -> ()
+  return
+}
+```
+
+The Pass will change the IR to:
+
+```
+// -----// IR Dump After {anonymous}::InsertGPUAllocs //----- //
+func.func @main() {
+  %c8 = arith.constant 8 : index
+  %c1 = arith.constant 1 : index
+  %cst = arith.constant 2.200000e+00 : f32
+  %cst_0 = arith.constant 1.100000e+00 : f32
+  %cst_1 = arith.constant 0.000000e+00 : f32
+  %memref = gpu.alloc  () {gpu.alloc_shared} : memref<8xf32>
+  %memref_2 = gpu.alloc  () {gpu.alloc_shared} : memref<8xf32>
+  %memref_3 = gpu.alloc  () {gpu.alloc_shared} : memref<8xf32>
+  %0 = memref.cast %memref : memref<8xf32> to memref<?xf32>
+  %1 = memref.cast %memref_2 : memref<8xf32> to memref<?xf32>
+  %2 = memref.cast %memref_3 : memref<8xf32> to memref<?xf32>
+  call @fillResource1DFloat(%0, %cst_0) : (memref<?xf32>, f32) -> ()
+  call @fillResource1DFloat(%1, %cst) : (memref<?xf32>, f32) -> ()
+  call @fillResource1DFloat(%2, %cst_1) : (memref<?xf32>, f32) -> ()
+  gpu.launch blocks(%arg0, %arg1, %arg2) in (%arg6 = %c8, %arg7 = %c1, %arg8 = %c1) threads(%arg3, %arg4, %arg5) in (%arg9 = %c1, %arg10 = %c1, %arg11 = %c1) {
+    %4 = gpu.block_id  x
+    %5 = memref.load %memref[%4] : memref<8xf32>
+    %6 = memref.load %memref_2[%4] : memref<8xf32>
+    %7 = arith.addf %5, %6 : f32
+    memref.store %7, %memref_3[%4] : memref<8xf32>
+    gpu.terminator
+  }
+  %3 = memref.cast %memref_3 : memref<8xf32> to memref<*xf32>
+  call @printMemrefF32(%3) : (memref<*xf32>) -> ()
+  return
+}
+```
+
+
+As shown in the example above, the memref.allocs in the IR are referring to device buffer allocation and hence they are replaced with gpu.alloc from the gpu dialect.
+
+
+## Reason for this Custom Pass:
+
+Upstream does not have a pass which does these conversions. Our goal is to add this pass to upstream which we think will be useful to the MLIR community.
---
+++ lib/Transforms/insert_gpu_allocs.cpp
@@ -0,0 +1,304 @@
+// Copyright 2022 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <mlir/Analysis/BufferViewFlowAnalysis.h>
+#include <mlir/Conversion/ArithmeticToSPIRV/ArithmeticToSPIRV.h>
+#include <mlir/Conversion/ControlFlowToSPIRV/ControlFlowToSPIRV.h>
+#include <mlir/Conversion/FuncToSPIRV/FuncToSPIRV.h>
+#include <mlir/Conversion/GPUToSPIRV/GPUToSPIRV.h>
+#include <mlir/Conversion/MathToSPIRV/MathToSPIRV.h>
+#include <mlir/Conversion/SCFToSPIRV/SCFToSPIRV.h>
+#include <mlir/Dialect/Affine/IR/AffineOps.h>
+#include <mlir/Dialect/ControlFlow/IR/ControlFlowOps.h>
+#include <mlir/Dialect/Func/IR/FuncOps.h>
+#include <mlir/Dialect/GPU/Transforms/ParallelLoopMapper.h>
+#include <mlir/Dialect/GPU/Transforms/Passes.h>
+#include <mlir/Dialect/MemRef/IR/MemRef.h>
+#include <mlir/Dialect/SCF/IR/SCF.h>
+#include <mlir/Dialect/SPIRV/IR/SPIRVDialect.h>
+#include <mlir/Dialect/SPIRV/IR/SPIRVOps.h>
+#include <mlir/Dialect/SPIRV/IR/TargetAndABI.h>
+#include <mlir/Dialect/SPIRV/Transforms/SPIRVConversion.h>
+#include <mlir/Pass/Pass.h>
+#include <mlir/Target/SPIRV/Serialization.h>
+#include <mlir/Transforms/DialectConversion.h>
+#include <mlir/Transforms/GreedyPatternRewriteDriver.h>
+
+#include <llvm/ADT/SmallBitVector.h>
+
+struct InsertGPUAllocs
+    : public mlir::PassWrapper<InsertGPUAllocs,
+                               mlir::OperationPass<mlir::func::FuncOp>> {
+
+  InsertGPUAllocs() = default;
+  InsertGPUAllocs(bool gpuDealloc) { useGpuDealloc = gpuDealloc; }
+  InsertGPUAllocs(const InsertGPUAllocs &pass) : PassWrapper(pass) {}
+
+  virtual void
+  getDependentDialects(mlir::DialectRegistry &registry) const override {
+    registry.insert<mlir::memref::MemRefDialect>();
+    registry.insert<mlir::gpu::GPUDialect>();
+    registry.insert<mlir::scf::SCFDialect>();
+    registry.insert<mlir::arith::ArithmeticDialect>();
+  }
+
+  void runOnOperation() override {
+    auto func = getOperation();
+    auto &funcBody = func.getBody();
+    if (funcBody.empty()) {
+      return;
+    } else if (!llvm::hasSingleElement(funcBody)) {
+      func.emitError("Function must have exactly one block");
+      signalPassFailure();
+      return;
+    }
+
+    struct AccessType {
+      bool hostRead = false;
+      bool hostWrite = false;
+      bool deviceRead = false;
+      bool deviceWrite = false;
+    };
+
+    llvm::SmallMapVector<mlir::Operation *, AccessType, 8> gpuBufferAllocs;
+    llvm::SmallMapVector<unsigned, AccessType, 8> gpuBufferParams;
+    auto &aliases = getAnalysis<mlir::BufferViewFlowAnalysis>();
+
+    auto getMemref = [](mlir::Operation *op)
+        -> llvm::Optional<mlir::SmallVector<mlir::Value, 4>> {
+      if (auto load = mlir::dyn_cast<mlir::memref::LoadOp>(op)) {
+        return {{load.memref()}};
+      } else if (auto store = mlir::dyn_cast<mlir::memref::StoreOp>(op)) {
+        return {{store.memref()}};
+      } else if (auto call = mlir::dyn_cast<mlir::func::CallOp>(op)) {
+        mlir::SmallVector<mlir::Value, 4> ret;
+        for (auto arg : call.operands()) {
+          if (arg.getType().isa<mlir::MemRefType>())
+            ret.emplace_back(arg);
+        }
+        return std::move(ret);
+      } else {
+        op->emitError("Uhhandled mem op in gpu region");
+        return llvm::None;
+      }
+    };
+
+    auto scfDialect = getContext().getOrLoadDialect<mlir::scf::SCFDialect>();
+
+    auto hasMemAccess = [](mlir::Operation *op) -> bool {
+      if (auto memInterface =
+              mlir::dyn_cast<mlir::MemoryEffectOpInterface>(op)) {
+        if (memInterface.hasEffect<mlir::MemoryEffects::Read>() ||
+            memInterface.hasEffect<mlir::MemoryEffects::Write>())
+          return true;
+      }
+      if (auto call = mlir::dyn_cast<mlir::func::CallOp>(op)) {
+        for (auto arg : call.operands()) {
+          if (arg.getType().isa<mlir::MemRefType>())
+            return true;
+        }
+      }
+      return false;
+    };
+
+    if (func.walk([&](mlir::Operation *op) {
+              if (!op->getParentOfType<mlir::gpu::LaunchOp>())
+                return mlir::WalkResult::advance();
+
+              if (!hasMemAccess(op))
+                return mlir::WalkResult::advance();
+
+              auto memref = getMemref(op);
+              if (!memref)
+                return mlir::WalkResult::interrupt();
+
+              for (auto mem : *memref) {
+                while (auto parentView =
+                           mem.getDefiningOp<mlir::ViewLikeOpInterface>())
+                  mem = parentView.getViewSource();
+
+                for (auto alias : aliases.resolve(mem)) {
+                  auto op = alias.getDefiningOp();
+                  if (op) {
+                    if (op->getDialect() == scfDialect ||
+                        mlir::isa<mlir::ViewLikeOpInterface>(op))
+                      continue;
+                    if (mlir::isa<mlir::memref::AllocOp,
+                                  mlir::memref::GetGlobalOp>(op)) {
+                      gpuBufferAllocs.insert({op, {}});
+                    } else if (mlir::isa<mlir::func::CallOp>(op)) {
+                      // Ignore
+                    } else {
+                      op->emitError("Unhandled memref producer");
+                      return mlir::WalkResult::interrupt();
+                    }
+
+                  } else {
+                    auto block = alias.getParentBlock();
+                    auto blockArgs = block->getArguments();
+                    auto it = llvm::find(blockArgs, alias);
+                    assert(it != blockArgs.end());
+                    auto index = static_cast<unsigned>(it - blockArgs.begin());
+                    gpuBufferParams.insert({index, {}});
+                  }
+                }
+              }
+
+              return mlir::WalkResult::advance();
+            })
+            .wasInterrupted()) {
+      signalPassFailure();
+      return;
+    }
+
+    auto getAccessType = [&](mlir::Value memref) {
+      AccessType ret;
+      for (auto mem : aliases.resolve(memref)) {
+        for (auto user : mem.getUsers()) {
+          if (mlir::isa<mlir::func::ReturnOp>(user)) {
+            ret.hostRead = true;
+            ret.hostWrite = true;
+            continue;
+          }
+
+          if (auto copy = mlir::dyn_cast<mlir::memref::CopyOp>(user)) {
+            if (copy.source() == mem)
+              ret.hostRead = true;
+
+            if (copy.target() == mem)
+              ret.hostWrite = true;
+
+            continue;
+          }
+
+          if (auto memInterface =
+                  mlir::dyn_cast<mlir::MemoryEffectOpInterface>(user)) {
+            bool onDevice = user->getParentOfType<mlir::gpu::LaunchOp>();
+            if (memInterface.hasEffect<mlir::MemoryEffects::Read>())
+              (onDevice ? ret.deviceRead : ret.hostRead) = true;
+
+            if (memInterface.hasEffect<mlir::MemoryEffects::Write>())
+              (onDevice ? ret.deviceWrite : ret.hostWrite) = true;
+
+            continue;
+          }
+          if (mlir::isa<mlir::func::CallOp>(user)) {
+            bool onDevice = user->getParentOfType<mlir::gpu::LaunchOp>();
+            (onDevice ? ret.deviceRead : ret.hostRead) = true;
+            (onDevice ? ret.deviceWrite : ret.hostWrite) = true;
+            continue;
+          }
+        }
+      }
+      return ret;
+    };
+
+    for (auto &it : gpuBufferAllocs) {
+      auto alloc = mlir::cast<mlir::memref::AllocOp>(it.first);
+      if (alloc)
+        it.second = getAccessType(alloc);
+      else {
+        auto memrefGlobal = mlir::cast<mlir::memref::GetGlobalOp>(it.first);
+        it.second = getAccessType(memrefGlobal);
+      }
+    }
+
+    auto &block = funcBody.front();
+    for (auto &it : gpuBufferParams) {
+      auto param = block.getArgument(it.first);
+      it.second = getAccessType(param);
+
+      it.second.hostRead = true;
+      it.second.hostWrite = true;
+    }
+
+    mlir::OpBuilder builder(func);
+    for (auto it : gpuBufferAllocs) {
+      auto alloc = mlir::cast<mlir::memref::AllocOp>(it.first);
+      auto access = it.second;
+      auto loc = alloc.getLoc();
+      builder.setInsertionPoint(alloc);
+      auto gpuAlloc = builder.create<mlir::gpu::AllocOp>(
+          loc, alloc.getType(), /*asyncToken*/ nullptr,
+          /*asyncDependencies*/ llvm::None, alloc.dynamicSizes(),
+          alloc.symbolOperands());
+      alloc->replaceAllUsesWith(gpuAlloc);
+      alloc.erase();
+      // if (access.hostRead || access.hostWrite)
+      //  gpuAlloc->setAttr(gpu_runtime::getAllocSharedAttrName(),
+      //                    builder.getUnitAttr());
+    }
+
+    auto term = block.getTerminator();
+    assert(term);
+
+    llvm::SmallVector<mlir::Value> dims;
+    llvm::SmallPtrSet<mlir::Operation *, 8> filter;
+    for (auto it : gpuBufferParams) {
+      auto param = block.getArgument(it.first);
+      auto access = it.second;
+      auto loc = param.getLoc();
+      builder.setInsertionPointToStart(&block);
+      auto memrefType = param.getType().cast<mlir::MemRefType>();
+      auto rank = static_cast<unsigned>(memrefType.getRank());
+      filter.clear();
+      dims.clear();
+      for (auto i : llvm::seq(0u, rank)) {
+        if (memrefType.isDynamicDim(i)) {
+          auto op = builder.create<mlir::memref::DimOp>(loc, param, i);
+          dims.push_back(op);
+          filter.insert(op);
+        }
+      }
+      auto allocType = mlir::MemRefType::get(
+          memrefType.getShape(), memrefType.getElementType(),
+          mlir::MemRefLayoutAttrInterface{}, memrefType.getMemorySpace());
+      auto gpuAlloc = builder.create<mlir::gpu::AllocOp>(
+          loc, allocType, /*asyncToken*/ nullptr,
+          /*asyncDependencies*/ llvm::None, dims,
+          /*symbolOperands*/ llvm::None);
+      auto allocResult = gpuAlloc.getResult(0);
+
+      // if (access.hostRead || access.hostWrite)
+      // gpuAlloc->setAttr(gpu_runtime::getAllocSharedAttrName(),
+      //                   builder.getUnitAttr());
+
+      if (access.hostWrite && access.deviceRead) {
+        auto copy =
+            builder.create<mlir::memref::CopyOp>(loc, param, allocResult);
+        filter.insert(copy);
+      }
+
+      if (allocType != memrefType)
+        allocResult =
+            builder.create<mlir::memref::CastOp>(loc, memrefType, allocResult);
+
+      param.replaceAllUsesExcept(allocResult, filter);
+      builder.setInsertionPoint(term);
+      if (access.hostRead && access.deviceWrite)
+        builder.create<mlir::memref::CopyOp>(loc, allocResult, param);
+
+      if (useGpuDealloc)
+        builder.create<mlir::gpu::DeallocOp>(loc, llvm::None, allocResult);
+      else
+        builder.create<mlir::memref::DeallocOp>(loc, allocResult);
+    }
+  }
+
+  Option<bool> useGpuDealloc{
+      *this, "use-gpu-dealloc",
+      llvm::cl::desc("use gpu.dealloc for gpu allocated memrefs, "
+                     "memref.dealloc will be used otherwise"),
+      llvm::cl::init(true)};
+};
