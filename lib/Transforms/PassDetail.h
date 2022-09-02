855ae194d623a49c342b000185cf86614cbef1ed
Dewei Wang <dewei.wang@intel.com>
2022-09-03 01:05:24 +0800

    add more comment and documentation

--- lib/Transforms/PassDetail.h
+++ lib/Transforms/PassDetail.h
@@ -1,6 +1,7 @@
 //===- PassDetail.h - Transforms Pass class details -------------*- C++ -*-===//
 //
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// Copyright 2022 Intel Corporation
+// Part of the IMEX Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
--- lib/Transforms/SerializeSPIRV.cpp
+++ lib/Transforms/SerializeSPIRV.cpp
@@ -1,13 +1,17 @@
 //===- SerializeSPIRV.cpp - SPIR-V serialize pass --------------*- C++ -*-===//
 //
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// Copyright 2022 Intel Corporation
+// Part of the IMEX Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
-//
-// serialize MLIR SPIR-V module to SPIR-V binary
-//
+///
+/// \file
+/// This pass iterates all the SPIR-V modules in the top module and serializes
+/// each SPIR-V module to SPIR-V binary and then attachs the binary blob as a
+/// string attribute to the corresponding gpu module.
+///
 //===----------------------------------------------------------------------===//
 
 #include "PassDetail.h"
@@ -28,6 +32,8 @@ public:
     llvm::SmallVector<uint32_t, 0> spvBinary;
     for (auto gpuMod : mod.getOps<gpu::GPUModuleOp>()) {
       auto name = gpuMod.getName();
+      // check that the spv module has the same name with gpu module except the
+      // prefix "__spv__"
       auto isSameMod = [&](spirv::ModuleOp spvMod) -> bool {
         auto spvModName = spvMod.getName();
         return spvModName->consume_front("__spv__") && spvModName == name;
@@ -42,12 +48,14 @@ public:
       auto spvMod = *it;
 
       spvBinary.clear();
+      // serialize the spv module to spv binary
       if (mlir::failed(spirv::serialize(spvMod, spvBinary))) {
         spvMod.emitError() << "Failed to serialize SPIR-V module";
         signalPassFailure();
         return;
       }
 
+      // attach the spv binary to the gpu module
       auto spvData =
           llvm::StringRef(reinterpret_cast<const char *>(spvBinary.data()),
                           spvBinary.size() * sizeof(uint32_t));
