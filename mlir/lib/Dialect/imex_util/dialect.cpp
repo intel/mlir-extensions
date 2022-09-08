e292c38d75b1c826c784d49a916749228b0da994
Ivan Butygin <ivan.butygin@gmail.com>
2022-09-09 00:55:58 +0200

    Add env region canonicalizations (#311)

--- mlir/lib/Dialect/imex_util/dialect.cpp
+++ mlir/lib/Dialect/imex_util/dialect.cpp
@@ -1712,6 +1712,251 @@ void EnvironmentRegionOp::getSuccessorRegions(
   assert(*index == 0 && "EnvironmentRegionOp must have single region");
   regions.push_back(mlir::RegionSuccessor(getResults()));
 }
+
+/// Propagate yielded values, defined outside region.
+struct EnvRegionPropagateOutsideValues
+    : public mlir::OpRewritePattern<EnvironmentRegionOp> {
+  using OpRewritePattern::OpRewritePattern;
+
+  mlir::LogicalResult
+  matchAndRewrite(EnvironmentRegionOp op,
+                  mlir::PatternRewriter &rewriter) const override {
+    auto oldResults = op.getResults();
+    auto count = static_cast<unsigned>(oldResults.size());
+
+    mlir::Block *body = &op.getRegion().front();
+    auto term = mlir::cast<EnvironmentRegionYieldOp>(body->getTerminator());
+    auto termArgs = term.results();
+    assert(oldResults.size() == termArgs.size());
+
+    // Build list of propagated and new yield args.
+    llvm::SmallVector<mlir::Value> newResults(count);
+    llvm::SmallVector<mlir::Value> newYieldArgs;
+    for (auto i : llvm::seq(0u, count)) {
+      auto arg = termArgs[i];
+      if (!op.getRegion().isAncestor(arg.getParentRegion())) {
+        // Value defined outside op region - use it directly instead of
+        // yielding.
+        newResults[i] = arg;
+      } else {
+        newYieldArgs.emplace_back(arg);
+      }
+    }
+
+    // Same yield results count - nothing changed.
+    if (newYieldArgs.size() == count)
+      return mlir::failure();
+
+    // Contruct new env region op, only yielding values that weren't propagated.
+    mlir::ValueRange newYieldArgsRange(newYieldArgs);
+    auto newOp = rewriter.create<EnvironmentRegionOp>(
+        op->getLoc(), newYieldArgsRange.getTypes(), op.environment(),
+        op.args());
+    mlir::Region &newRegion = newOp.getRegion();
+    rewriter.inlineRegionBefore(op.getRegion(), newRegion, newRegion.end());
+    {
+      mlir::OpBuilder::InsertionGuard g(rewriter);
+      rewriter.setInsertionPoint(term);
+      rewriter.replaceOpWithNewOp<EnvironmentRegionYieldOp>(term, newYieldArgs);
+    }
+
+    // Fill results that weren't propagated with results of new op.
+    for (auto i : llvm::seq(0u, count)) {
+      if (!newResults[i]) {
+        newResults[i] = newYieldArgsRange.front();
+        newYieldArgsRange = newYieldArgsRange.drop_front();
+      }
+    }
+    assert(newYieldArgsRange.empty() &&
+           "Some values weren't consumed - yield args count mismatch?");
+
+    rewriter.replaceOp(op, newResults);
+    return mlir::success();
+  }
+};
+
+/// Merge nested env region if parent have same environment and args.
+struct MergeNestedEnvRegion
+    : public mlir::OpRewritePattern<EnvironmentRegionOp> {
+  using OpRewritePattern::OpRewritePattern;
+
+  mlir::LogicalResult
+  matchAndRewrite(EnvironmentRegionOp op,
+                  mlir::PatternRewriter &rewriter) const override {
+    auto parent = op->getParentOfType<EnvironmentRegionOp>();
+    if (!parent)
+      return mlir::failure();
+
+    if (parent.environment() != op.environment() || parent.args() != op.args())
+      return mlir::failure();
+
+    EnvironmentRegionOp::inlineIntoParent(rewriter, op);
+    return mlir::success();
+  }
+};
+
+/// Remove duplicated and unused env region yield args.
+struct CleanupRegionYieldArgs
+    : public mlir::OpRewritePattern<EnvironmentRegionOp> {
+  using OpRewritePattern::OpRewritePattern;
+
+  mlir::LogicalResult
+  matchAndRewrite(EnvironmentRegionOp op,
+                  mlir::PatternRewriter &rewriter) const override {
+    mlir::Block *body = &op.getRegion().front();
+    auto term = mlir::cast<EnvironmentRegionYieldOp>(body->getTerminator());
+
+    auto results = op.getResults();
+    auto yieldArgs = term.results();
+    assert(results.size() == yieldArgs.size());
+    auto count = static_cast<unsigned>(results.size());
+
+    // Build new yield args list, and mapping between old and new results
+    llvm::SmallVector<mlir::Value> newYieldArgs;
+    llvm::SmallVector<int> newResultsMapping(count, -1);
+    llvm::SmallDenseMap<mlir::Value, int> argsMap;
+    for (auto i : llvm::seq(0u, count)) {
+      auto res = results[i];
+
+      // Unused result.
+      if (res.getUses().empty())
+        continue;
+
+      auto arg = yieldArgs[i];
+      auto it = argsMap.find_as(arg);
+      if (it == argsMap.end()) {
+        // Add new result, compute index mapping for it.
+        auto ind = static_cast<int>(newYieldArgs.size());
+        argsMap.insert({arg, ind});
+        newYieldArgs.emplace_back(arg);
+        newResultsMapping[i] = ind;
+      } else {
+        // Duplicated result, reuse prev result index.
+        newResultsMapping[i] = it->second;
+      }
+    }
+
+    // Same yield results count - nothing changed.
+    if (newYieldArgs.size() == count)
+      return mlir::failure();
+
+    // Contruct new env region op, only yielding values we selected.
+    mlir::ValueRange newYieldArgsRange(newYieldArgs);
+    auto newOp = rewriter.create<EnvironmentRegionOp>(
+        op->getLoc(), newYieldArgsRange.getTypes(), op.environment(),
+        op.args());
+    mlir::Region &newRegion = newOp.getRegion();
+    rewriter.inlineRegionBefore(op.getRegion(), newRegion, newRegion.end());
+    {
+      mlir::OpBuilder::InsertionGuard g(rewriter);
+      rewriter.setInsertionPoint(term);
+      rewriter.replaceOpWithNewOp<EnvironmentRegionYieldOp>(term, newYieldArgs);
+    }
+
+    // Contruct new result list, using mapping previously constructed.
+    auto newResults = newOp.getResults();
+    llvm::SmallVector<mlir::Value> newResultsToTeplace(count);
+    for (auto i : llvm::seq(0u, count)) {
+      auto mapInd = newResultsMapping[i];
+      if (mapInd != -1)
+        newResultsToTeplace[i] = newResults[mapInd];
+    }
+
+    rewriter.replaceOp(op, newResultsToTeplace);
+    return mlir::success();
+  }
+};
+
+/// Merge adjacent env regions.
+struct MergeAdjacentRegions
+    : public mlir::OpRewritePattern<EnvironmentRegionOp> {
+  using OpRewritePattern::OpRewritePattern;
+
+  mlir::LogicalResult
+  matchAndRewrite(EnvironmentRegionOp op,
+                  mlir::PatternRewriter &rewriter) const override {
+    // Get next pos and check if it is also env region op, current op cannot be
+    // last as it is not a terminator.
+    auto opPos = op->getIterator();
+    auto nextOp = mlir::dyn_cast<EnvironmentRegionOp>(*std::next(opPos));
+    if (!nextOp)
+      return mlir::failure();
+
+    if (nextOp.environment() != op.environment() || nextOp.args() != op.args())
+      return mlir::failure();
+
+    mlir::Block *body = &op.getRegion().front();
+    auto term = mlir::cast<EnvironmentRegionYieldOp>(body->getTerminator());
+
+    auto results = op.getResults();
+    auto yieldArgs = term.results();
+    assert(results.size() == yieldArgs.size());
+    auto count = static_cast<unsigned>(results.size());
+
+    // Check if any results from first op are being used in second one, we need
+    // to replace them by direct values.
+    for (auto i : llvm::seq(0u, count)) {
+      auto res = results[i];
+      for (auto &use : llvm::make_early_inc_range(res.getUses())) {
+        auto *owner = use.getOwner();
+        if (nextOp->isProperAncestor(owner)) {
+          auto arg = yieldArgs[i];
+          rewriter.updateRootInPlace(owner, [&]() { use.set(arg); });
+        }
+      }
+    }
+
+    mlir::Block *nextBody = &nextOp.getRegion().front();
+    auto nextTerm =
+        mlir::cast<EnvironmentRegionYieldOp>(nextBody->getTerminator());
+    auto nextYieldArgs = nextTerm.results();
+
+    // Contruct merged yield args list, some of the results may become unused,
+    // but they will be cleaned up by other pattern.
+    llvm::SmallVector<mlir::Value> newYieldArgs(count + nextYieldArgs.size());
+    llvm::copy(nextYieldArgs, llvm::copy(yieldArgs, newYieldArgs.begin()));
+
+    {
+      // Merge region from second op into ferst one.
+      mlir::OpBuilder::InsertionGuard g(rewriter);
+      rewriter.mergeBlockBefore(nextBody, term);
+      rewriter.setInsertionPoint(term);
+      rewriter.create<EnvironmentRegionYieldOp>(term->getLoc(), newYieldArgs);
+      rewriter.eraseOp(term);
+      rewriter.eraseOp(nextTerm);
+    }
+
+    // Contruct new env region op and steal new merged region into it.
+    mlir::ValueRange newYieldArgsRange(newYieldArgs);
+    auto newOp = rewriter.create<EnvironmentRegionOp>(
+        op->getLoc(), newYieldArgsRange.getTypes(), op.environment(),
+        op.args());
+    mlir::Region &newRegion = newOp.getRegion();
+    rewriter.inlineRegionBefore(op.getRegion(), newRegion, newRegion.end());
+
+    auto newResults = newOp.getResults();
+
+    rewriter.replaceOp(op, newResults.take_front(count));
+    rewriter.replaceOp(nextOp, newResults.drop_front(count));
+    return mlir::success();
+  }
+};
+
+void EnvironmentRegionOp::getCanonicalizationPatterns(
+    mlir::RewritePatternSet &results, mlir::MLIRContext *context) {
+  results.insert<EnvRegionPropagateOutsideValues, MergeNestedEnvRegion,
+                 CleanupRegionYieldArgs, MergeAdjacentRegions>(context);
+}
+
+void EnvironmentRegionOp::inlineIntoParent(mlir::PatternRewriter &builder,
+                                           EnvironmentRegionOp op) {
+  mlir::Block *block = &op.getRegion().front();
+  auto term = mlir::cast<EnvironmentRegionYieldOp>(block->getTerminator());
+  auto args = llvm::to_vector(term.results());
+  builder.eraseOp(term);
+  builder.mergeBlockBefore(block, op);
+  builder.replaceOp(op, args);
+}
 } // namespace util
 } // namespace imex
 
--- mlir/test/Dialect/imex_util/canonicalize.mlir
+++ mlir/test/Dialect/imex_util/canonicalize.mlir
@@ -1,4 +1,4 @@
-// RUN: imex-opt %s -canonicalize --split-input-file | FileCheck %s
+// RUN: imex-opt %s -allow-unregistered-dialect -canonicalize --split-input-file | FileCheck %s
 
 func.func @test(%arg1: index, %arg2: i64) -> i64 {
   %0 = imex_util.build_tuple %arg1, %arg2: index, i64 -> tuple<index, i64>
@@ -9,3 +9,110 @@ func.func @test(%arg1: index, %arg2: i64) -> i64 {
 // CHECK-LABEL: func @test
 //  CHECK-SAME:   (%[[ARG1:.*]]: index, %[[ARG2:.*]]: i64)
 //  CHECK-NEXT:   return %[[ARG2]] : i64
+
+// -----
+
+func.func @remove_empty_region() {
+  imex_util.env_region "test" {
+  }
+  return
+}
+// CHECK-LABEL: func @remove_empty_region
+//   CHECK-NOT:   imex_util.env_region
+//  CHECK-NEXT:   return
+
+// -----
+
+func.func @empty_region_out_value(%arg1: index) -> index {
+  %0 = imex_util.env_region "test" -> index {
+    imex_util.env_region_yield %arg1: index
+  }
+  return %0 : index
+}
+// CHECK-LABEL: func @empty_region_out_value
+//  CHECK-SAME: (%[[ARG:.*]]: index)
+//       CHECK: return %[[ARG]] : index
+
+// -----
+
+func.func @merge_nested_region() {
+  imex_util.env_region "test" {
+    "test.test1"() : () -> ()
+    imex_util.env_region "test" {
+      "test.test2"() : () -> ()
+    }
+    "test.test3"() : () -> ()
+  }
+  return
+}
+// CHECK-LABEL: func @merge_nested_region
+//  CHECK-NEXT:   imex_util.env_region
+//  CHECK-NEXT:   "test.test1"() : () -> ()
+//  CHECK-NEXT:   "test.test2"() : () -> ()
+//  CHECK-NEXT:   "test.test3"() : () -> ()
+//  CHECK-NEXT:   }
+//  CHECK-NEXT:   return
+
+// -----
+
+func.func @nested_region_yield_args() {
+  %0:4 = imex_util.env_region "test" -> index, i32, index, i64 {
+    %1:3 = "test.test1"() : () -> (index, i32, i64)
+    imex_util.env_region_yield %1#0, %1#1, %1#0, %1#2: index, i32, index, i64
+  }
+  "test.test2"(%0#0, %0#2, %0#3) : (index, index, i64) -> ()
+  return
+}
+// CHECK-LABEL: func @nested_region_yield_args
+//  CHECK-NEXT:   %[[RES:.*]]:2 = imex_util.env_region "test" -> index, i64 {
+//  CHECK-NEXT:   %[[VAL:.*]]:3 = "test.test1"() : () -> (index, i32, i64)
+//  CHECK-NEXT:   imex_util.env_region_yield %[[VAL]]#0, %[[VAL]]#2 : index, i64
+//  CHECK-NEXT:   }
+//  CHECK-NEXT:   "test.test2"(%[[RES]]#0, %[[RES]]#0, %[[RES]]#1) : (index, index, i64) -> ()
+//  CHECK-NEXT:   return
+
+// -----
+
+func.func @merge_adjacent_region1() {
+  imex_util.env_region "test" {
+    "test.test1"() : () -> ()
+  }
+  imex_util.env_region "test" {
+    "test.test2"() : () -> ()
+  }
+  imex_util.env_region "test" {
+    "test.test3"() : () -> ()
+  }
+  return
+}
+// CHECK-LABEL: func @merge_adjacent_region1
+//  CHECK-NEXT:   imex_util.env_region
+//  CHECK-NEXT:   "test.test1"() : () -> ()
+//  CHECK-NEXT:   "test.test2"() : () -> ()
+//  CHECK-NEXT:   "test.test3"() : () -> ()
+//  CHECK-NEXT:   }
+//  CHECK-NEXT:   return
+
+// -----
+
+func.func @merge_adjacent_region2() {
+  %0 = imex_util.env_region "test" -> index {
+    %1 = "test.test1"() : () -> index
+    imex_util.env_region_yield %1: index
+  }
+  %2 = imex_util.env_region "test" -> i64 {
+    %3 = "test.test2"(%0) : (index) -> i64
+    imex_util.env_region_yield %3: i64
+  }
+  imex_util.env_region "test" {
+    "test.test3"(%2) : (i64) -> ()
+  }
+  return
+}
+// CHECK-LABEL: func @merge_adjacent_region2
+//  CHECK-NEXT:   imex_util.env_region
+//  CHECK-NEXT:   %[[VAL1:.*]] = "test.test1"() : () -> index
+//  CHECK-NEXT:   %[[VAL2:.*]] = "test.test2"(%[[VAL1]]) : (index) -> i64
+//  CHECK-NEXT:   "test.test3"(%[[VAL2]]) : (i64) -> ()
+//  CHECK-NEXT:   }
+//  CHECK-NEXT:   return
