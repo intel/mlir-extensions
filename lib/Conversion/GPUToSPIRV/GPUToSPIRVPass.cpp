e4356a64aa2a8581cd1dad3d2e960701783bdfd8
Dimple Prajapati <dimpalben.r.prajapati@intel.com>
2022-09-09 09:44:44 -0700

    Port GPUToSPIRV Pass
    This pass extends upstream GPUToSPIRV pass by adding more conversion patterns to include
    SCF, arith, math ops within gpu kernel module.

---
+++ lib/Conversion/GPUToSPIRV/GPUToSPIRVPass.cpp
@@ -0,0 +1,102 @@
+//===- GPUToSPIRVPass.cpp -  --------------*- C++ -*-===//
+//
+// Copyright 2022 Intel Corporation
+// Part of the IMEX Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file extends upstream GPUToSPIRV Pass that converts GPU ops to SPIR-V
+/// by by adding more conversion patterns like SCF, math and control flow. This
+/// pass only converts gpu.func ops inside gpu.module op.
+///
+//===----------------------------------------------------------------------===//
+#include "imex/Conversion/GPUToSPIRV/GPUToSPIRVPass.h"
+
+#include "../PassDetail.h"
+#include "mlir/Conversion/MemRefToSPIRV/MemRefToSPIRV.h"
+#include "mlir/Dialect/GPU/IR/GPUDialect.h"
+
+#include <mlir/Analysis/BufferViewFlowAnalysis.h>
+#include <mlir/Conversion/ArithmeticToSPIRV/ArithmeticToSPIRV.h>
+#include <mlir/Conversion/ControlFlowToSPIRV/ControlFlowToSPIRV.h>
+#include <mlir/Conversion/FuncToSPIRV/FuncToSPIRV.h>
+#include <mlir/Conversion/GPUToSPIRV/GPUToSPIRV.h>
+#include <mlir/Conversion/MathToSPIRV/MathToSPIRV.h>
+#include <mlir/Conversion/SCFToSPIRV/SCFToSPIRV.h>
+#include <mlir/Dialect/Affine/IR/AffineOps.h>
+#include <mlir/Dialect/ControlFlow/IR/ControlFlowOps.h>
+#include <mlir/Dialect/Func/IR/FuncOps.h>
+#include <mlir/Dialect/GPU/Transforms/ParallelLoopMapper.h>
+#include <mlir/Dialect/GPU/Transforms/Passes.h>
+#include <mlir/Dialect/MemRef/IR/MemRef.h>
+#include <mlir/Dialect/SCF/IR/SCF.h>
+#include <mlir/Dialect/SPIRV/IR/SPIRVDialect.h>
+#include <mlir/Dialect/SPIRV/IR/SPIRVOps.h>
+#include <mlir/Dialect/SPIRV/IR/TargetAndABI.h>
+#include <mlir/Dialect/SPIRV/Transforms/SPIRVConversion.h>
+#include <mlir/Pass/Pass.h>
+#include <mlir/Target/SPIRV/Serialization.h>
+#include <mlir/Transforms/DialectConversion.h>
+
+namespace imex {
+
+/// Pass to lower GPU Dialect to SPIR-V. The pass only converts the gpu.func ops
+/// inside gpu.module ops. i.e., the function that are referenced in
+/// gpu.launch_func ops. For each such function
+///
+/// 1) Create a spirv::ModuleOp, and clone the function into spirv::ModuleOp
+/// (the original function is still needed by the gpu::LaunchKernelOp, so cannot
+/// replace it).
+///
+/// 2) Lower the body of the spirv::ModuleOp.
+struct ConvertGPUXToSPIRVPass
+    : public ::imex::ConvertGPUXToSPIRVBase<ConvertGPUXToSPIRVPass> {
+
+  void runOnOperation() override {
+    mlir::MLIRContext *context = &getContext();
+    mlir::ModuleOp module = getOperation();
+
+    llvm::SmallVector<mlir::Operation *, 1> kernelModules;
+    mlir::OpBuilder builder(context);
+    module.walk([&builder, &kernelModules](mlir::gpu::GPUModuleOp moduleOp) {
+      // For each kernel module (should be only 1 for now, but that is not a
+      // requirement here), clone the module for conversion because the
+      // gpu.launch function still needs the kernel module.
+      builder.setInsertionPoint(moduleOp.getOperation());
+      kernelModules.push_back(builder.clone(*moduleOp.getOperation()));
+    });
+
+    auto targetAttr = mlir::spirv::lookupTargetEnvOrDefault(module);
+    std::unique_ptr<mlir::ConversionTarget> target =
+        mlir::SPIRVConversionTarget::get(targetAttr);
+
+    mlir::SPIRVTypeConverter typeConverter(targetAttr);
+    mlir::RewritePatternSet patterns(context);
+
+    //------- Upstream Conversion------------
+    mlir::populateGPUToSPIRVPatterns(typeConverter, patterns);
+    mlir::arith::populateArithmeticToSPIRVPatterns(typeConverter, patterns);
+    mlir::populateMemRefToSPIRVPatterns(typeConverter, patterns);
+    mlir::populateFuncToSPIRVPatterns(typeConverter, patterns);
+    // ---------------------------------------
+
+    // IMEX GPUToSPIRV extension
+    mlir::ScfToSPIRVContext scfToSpirvCtx;
+    mlir::populateSCFToSPIRVPatterns(typeConverter, scfToSpirvCtx, patterns);
+    mlir::cf::populateControlFlowToSPIRVPatterns(typeConverter, patterns);
+    mlir::populateMathToSPIRVPatterns(typeConverter, patterns);
+
+    if (failed(
+            applyFullConversion(kernelModules, *target, std::move(patterns))))
+      return signalPassFailure();
+  }
+};
+
+std::unique_ptr<::mlir::OperationPass<::mlir::ModuleOp>>
+createConvertGPUXToSPIRVPass() {
+  return std::make_unique<ConvertGPUXToSPIRVPass>();
+}
+} // namespace imex
---
+++ test/Conversion/GPUToSPIRV/scf.mlir
@@ -0,0 +1,67 @@
+// RUN: imex-opt -allow-unregistered-dialect -convert-gpux-to-spirv -verify-diagnostics %s -o - | FileCheck %s
+
+module attributes {
+    gpu.container_module,
+    spv.target_env = #spv.target_env<
+    #spv.vce<v1.0, [Shader], [SPV_KHR_storage_buffer_storage_class]>, #spv.resource_limits<>>
+} {
+  gpu.module @kernels {
+    // CHECK:       spv.module @{{.*}} Logical GLSL450 {
+    // CHECK-LABEL: spv.func @loop_kernel
+    // CHECK-SAME: {{%.*}}: !spv.ptr<!spv.struct<(!spv.array<10 x f32, stride=4> [0])>, StorageBuffer> {spv.interface_var_abi = #spv.interface_var_abi<(0, 0)>}
+    // CHECK-SAME: {{%.*}}: !spv.ptr<!spv.struct<(!spv.array<10 x f32, stride=4> [0])>, StorageBuffer> {spv.interface_var_abi = #spv.interface_var_abi<(0, 1)>}
+    // CHECK-SAME: spv.entry_point_abi = #spv.entry_point_abi<local_size = dense<[32, 4, 1]> : vector<3xi32>>
+    gpu.func @loop_kernel(%arg2 : memref<10xf32>, %arg3 : memref<10xf32>) kernel
+      attributes {spv.entry_point_abi = #spv.entry_point_abi<local_size = dense<[32, 4, 1]>: vector<3xi32>>} {
+      // CHECK: spv.Branch ^bb1
+      // CHECK: ^bb1:  // pred: ^bb0
+       cf.br ^bb1
+      ^bb1:  // pred: ^bb0
+      // CHECK: %[[LB:.*]] = spv.Constant 4 : i32
+        %lb = arith.constant 4 : index
+        // CHECK: %[[UB:.*]] = spv.Constant 42 : i32
+        %ub = arith.constant 42 : index
+        // CHECK: %[[STEP:.*]] = spv.Constant 2 : i32
+        %step = arith.constant 2 : index
+        // CHECK: spv.mlir.loop {
+        // CHECK-NEXT: spv.Branch ^[[HEADER:.*]](%[[LB]] : i32)
+        // CHECK:      ^[[HEADER]](%[[INDVAR:.*]]: i32):
+        // CHECK:        %[[CMP:.*]] = spv.SLessThan %[[INDVAR]], %[[UB]] : i32
+        // CHECK:        spv.BranchConditional %[[CMP]], ^[[BODY:.*]], ^[[MERGE:.*]]
+        // CHECK:      ^[[BODY]]:
+        // CHECK:        %[[ZERO1:.*]] = spv.Constant 0 : i32
+        // CHECK:        %[[OFFSET1:.*]] = spv.Constant 0 : i32
+        // CHECK:        %[[STRIDE1:.*]] = spv.Constant 1 : i32
+        // CHECK:        %[[UPDATE1:.*]] = spv.IMul %[[STRIDE1]], %[[INDVAR]] : i32
+        // CHECK:        %[[INDEX1:.*]] = spv.IAdd %[[OFFSET1]], %[[UPDATE1]] : i32
+        // CHECK:        spv.AccessChain {{%.*}}{{\[}}%[[ZERO1]], %[[INDEX1]]{{\]}}
+        // CHECK:        %[[ZERO2:.*]] = spv.Constant 0 : i32
+        // CHECK:        %[[OFFSET2:.*]] = spv.Constant 0 : i32
+        // CHECK:        %[[STRIDE2:.*]] = spv.Constant 1 : i32
+        // CHECK:        %[[UPDATE2:.*]] = spv.IMul %[[STRIDE2]], %[[INDVAR]] : i32
+        // CHECK:        %[[INDEX2:.*]] = spv.IAdd %[[OFFSET2]], %[[UPDATE2]] : i32
+        // CHECK:        spv.AccessChain {{%.*}}[%[[ZERO2]], %[[INDEX2]]]
+        // CHECK:        %[[INCREMENT:.*]] = spv.IAdd %[[INDVAR]], %[[STEP]] : i32
+        // CHECK:        spv.Branch ^[[HEADER]](%[[INCREMENT]] : i32)
+        // CHECK:      ^[[MERGE]]
+        // CHECK:        spv.mlir.merge
+        // CHECK:      }
+        scf.for %arg4 = %lb to %ub step %step {
+             %1 = memref.load %arg2[%arg4] : memref<10xf32>
+            memref.store %1, %arg3[%arg4] : memref<10xf32>
+        }
+      // CHECK: spv.Return
+      gpu.return
+    }
+  }
+
+  func.func @main() {
+    %0 = "op"() : () -> (memref<10xf32>)
+    %1 = "op"() : () -> (memref<10xf32>)
+    %cst = arith.constant 1 : index
+    gpu.launch_func @kernels::@loop_kernel
+        blocks in (%cst, %cst, %cst) threads in (%cst, %cst, %cst)
+        args(%0 : memref<10xf32>, %1 : memref<10xf32>)
+    return
+  }
+}
