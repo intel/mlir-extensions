7264ccfb90c1bda067798ad18197f4babe6bd1b1
Ivan Butygin <ivan.butygin@gmail.com>
2022-10-04 00:54:00 +0200

    pass

---
+++ mlir/lib/Dialect/gpu_runtime/Transforms/MakeBarriersUniform.cpp
@@ -0,0 +1,171 @@
+// Copyright 2022 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "imex/Dialect/gpu_runtime/Transforms/MakeBarriersUniform.hpp"
+
+#include "imex/Dialect/gpu_runtime/IR/gpu_runtime_ops.hpp"
+#include "imex/Dialect/imex_util/dialect.hpp"
+
+#include <mlir/Dialect/GPU/IR/GPUDialect.h>
+#include <mlir/Dialect/SCF/IR/SCF.h>
+#include <mlir/IR/Dominance.h>
+#include <mlir/Pass/Pass.h>
+#include <mlir/Transforms/GreedyPatternRewriteDriver.h>
+
+namespace {
+struct ConvertBarrierOp
+    : public mlir::OpRewritePattern<gpu_runtime::GPUBarrierOp> {
+  using OpRewritePattern::OpRewritePattern;
+
+  mlir::LogicalResult
+  matchAndRewrite(gpu_runtime::GPUBarrierOp op,
+                  mlir::PatternRewriter &rewriter) const override {
+    auto launchOp = op->getParentOfType<mlir::gpu::LaunchOp>();
+
+    // Must be within launch op.
+    if (!launchOp)
+      return mlir::failure();
+
+    // If op must be an immediate parent
+    auto ifOp = mlir::dyn_cast<mlir::scf::IfOp>(op->getParentOp());
+    if (!ifOp)
+      return mlir::failure();
+
+    // Launch op must be an immediate parent of ifOp.
+    if (ifOp->getParentOp() != launchOp)
+      return mlir::failure();
+
+    // IfOp with else block is not yet supported;
+    if (ifOp.elseBlock())
+      return mlir::failure();
+
+    mlir::Block *ifBody = ifOp.thenBlock();
+    assert(ifBody);
+
+    mlir::DominanceInfo dom;
+    llvm::SmallMapVector<mlir::Value, unsigned, 8> yieldArgsMap;
+
+    auto barrierIt = op->getIterator();
+    for (auto &beforeOp : llvm::make_range(ifBody->begin(), barrierIt)) {
+      for (auto result : beforeOp.getResults()) {
+        for (mlir::OpOperand user : result.getUsers()) {
+          auto owner = user.getOwner();
+          if (dom.properlyDominates(op, owner)) {
+            auto idx = static_cast<unsigned>(yieldArgsMap.size());
+            yieldArgsMap.insert({result, idx});
+          }
+        }
+      }
+    }
+
+    auto yieldArgs = [&]() {
+      auto range = llvm::make_first_range(yieldArgsMap);
+      return llvm::SmallVector<mlir::Value>(std::begin(range), std::end(range));
+    }();
+
+    auto afterBlock = rewriter.splitBlock(ifBody, std::next(barrierIt));
+
+    auto barrierLoc = op->getLoc();
+    auto barrierFlags = op.flags();
+    rewriter.eraseOp(op);
+
+    rewriter.setInsertionPointToEnd(ifBody);
+    rewriter.create<mlir::scf::YieldOp>(rewriter.getUnknownLoc(), yieldArgs);
+
+    rewriter.setInsertionPoint(ifOp);
+    auto ifLoc = ifOp->getLoc();
+    auto cond = ifOp.getCondition();
+
+    auto elseBodyBuilder = [&](mlir::OpBuilder &builder, mlir::Location loc) {
+      llvm::SmallVector<mlir::Value> results;
+      results.reserve(yieldArgs.size());
+      for (auto arg : yieldArgs)
+        builder.create<imex::util::UndefOp>(loc, arg.getType());
+
+      builder.create<mlir::scf::YieldOp>(loc, results);
+    };
+
+    mlir::ValueRange yieldArgsRange(yieldArgs);
+    auto beforeIf = rewriter.create<mlir::scf::IfOp>(
+        ifLoc, yieldArgsRange.getTypes(), cond, nullptr, elseBodyBuilder);
+    auto &beforeIfThenReg = beforeIf.getThenRegion();
+    rewriter.inlineRegionBefore(ifOp.getThenRegion(), beforeIfThenReg,
+                                beforeIfThenReg.end());
+
+    rewriter.create<gpu_runtime::GPUBarrierOp>(barrierLoc, barrierFlags);
+
+    auto beforeIfResults = beforeIf.getResults();
+
+    auto thenBodyBuilder = [](mlir::OpBuilder & /*builder*/,
+                              mlir::Location /*loc*/) {
+      // Nothing
+    };
+
+    auto afterIf =
+        rewriter.create<mlir::scf::IfOp>(ifLoc, cond, thenBodyBuilder);
+    rewriter.mergeBlocks(afterBlock, afterIf.thenBlock());
+
+    for (auto &op : *afterIf.thenBlock()) {
+      for (mlir::OpOperand &arg : op.getOpOperands()) {
+        auto val = arg.get();
+        auto it = yieldArgsMap.find(val);
+        if (it != yieldArgsMap.end()) {
+          auto i = it->second;
+          assert(i < beforeIfResults.size() && "Invalid result index.");
+          auto newVal = beforeIfResults[i];
+          rewriter.updateRootInPlace(&op, [&]() { arg.set(newVal); });
+        }
+      }
+    }
+
+    rewriter.eraseOp(ifOp);
+    return mlir::success();
+  }
+};
+} // namespace
+
+void gpu_runtime::populateMakeBarriersUniformPatterns(
+    mlir::MLIRContext &context, mlir::RewritePatternSet &patterns) {
+  patterns.insert<ConvertBarrierOp>(&context);
+}
+
+namespace {
+struct MakeBarriersUniformPass
+    : public mlir::PassWrapper<MakeBarriersUniformPass,
+                               mlir::OperationPass<void>> {
+  MLIR_DEFINE_EXPLICIT_INTERNAL_INLINE_TYPE_ID(MakeBarriersUniformPass)
+
+  virtual void
+  getDependentDialects(mlir::DialectRegistry &registry) const override {
+    registry.insert<gpu_runtime::GpuRuntimeDialect>();
+    registry.insert<imex::util::ImexUtilDialect>();
+    registry.insert<mlir::gpu::GPUDialect>();
+    registry.insert<mlir::scf::SCFDialect>();
+  }
+
+  void runOnOperation() override {
+    auto &ctx = getContext();
+    mlir::RewritePatternSet patterns(&ctx);
+
+    gpu_runtime::populateMakeBarriersUniformPatterns(ctx, patterns);
+
+    (void)mlir::applyPatternsAndFoldGreedily(getOperation(),
+                                             std::move(patterns));
+  }
+};
+} // namespace
+
+std::unique_ptr<mlir::Pass> gpu_runtime::createMakeBarriersUniformPass() {
+  return std::make_unique<MakeBarriersUniformPass>();
+}
