3307bc38725a41681bad291d846a1390de294c86
Ivan Butygin <ivan.butygin@gmail.com>
2022-10-04 19:47:51 +0200

    more barrier test

--- numba_dpcomp/numba_dpcomp/mlir/tests/test_gpu.py
+++ numba_dpcomp/numba_dpcomp/mlir/tests/test_gpu.py
@@ -692,8 +692,8 @@ def test_input_load_cse():
 @require_gpu
 @pytest.mark.parametrize("op", [barrier, mem_fence])
 @pytest.mark.parametrize("flags", [CLK_LOCAL_MEM_FENCE, CLK_GLOBAL_MEM_FENCE])
-@pytest.mark.parametrize("global_size", [1, 2, 27])
-@pytest.mark.parametrize("local_size", [1, 2, 7])
+@pytest.mark.parametrize("global_size", [1, 2, 27, 67, 101])
+@pytest.mark.parametrize("local_size", [1, 2, 7, 17, 33])
 def test_barrier_ops(op, flags, global_size, local_size):
     atomic_add = atomic.add
 
