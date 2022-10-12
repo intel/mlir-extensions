7d2d9d5b268d05ec6d66a544d0a2050c587f56d3
Ivan Butygin <ivan.butygin@gmail.com>
2022-10-12 20:41:52 +0200

    TBB 2021.6 support (#366)

--- dpcomp_runtime/lib/tbb_parallel.cpp
+++ dpcomp_runtime/lib/tbb_parallel.cpp
@@ -32,25 +32,45 @@
 #define DEBUG 0
 
 namespace {
-static std::mutex &getDebugMutext() {
+static std::mutex &getDebugMutex() {
   static std::mutex mut;
   return mut;
 }
 
+static tbb::task_scheduler_handle tbbTshAttach() {
+#if TBB_INTERFACE_VERSION >= 12060
+  return tbb::attach();
+#else
+  return tbb::task_scheduler_handle::get();
+#endif
+}
+
+static void tbbTshRelease(tbb::task_scheduler_handle &tsh) {
+#if TBB_INTERFACE_VERSION >= 12060
+  tsh.release();
+#else
+  tbb::task_scheduler_handle::release(tsh);
+#endif
+}
+
 struct TBBContext {
   TBBContext(int numThreads)
-      : numThreads(numThreads),
-        scheduler_handle(tbb::task_scheduler_handle::get()), arena(numThreads) {
-
-  }
+      : numThreads(numThreads), schedulerHandle(tbbTshAttach()),
+        arena(numThreads) {}
 
   ~TBBContext() {
     arena.terminate();
-    (void)tbb::finalize(scheduler_handle, std::nothrow);
+    if (!tbb::finalize(schedulerHandle, std::nothrow)) {
+      if (DEBUG) {
+        fprintf(stderr, "dpcomp: failed to finalize tbb runtime\n");
+        fflush(stderr);
+      }
+      tbbTshRelease(schedulerHandle);
+    }
   }
 
   int numThreads;
-  tbb::task_scheduler_handle scheduler_handle;
+  tbb::task_scheduler_handle schedulerHandle;
   tbb::task_arena arena;
 };
 
@@ -81,30 +101,29 @@ struct Dim {
   Dim *prev;
 };
 
-using parallel_for_fptr = void (*)(const Range *, size_t, void *);
+using ParallelForFptr = void (*)(const Range *, size_t, void *);
 
-static void parallel_for_nested(const InputRange *input_ranges, size_t depth,
-                                size_t num_threads, size_t num_loops,
-                                Dim *prev_dim, parallel_for_fptr func,
-                                void *ctx);
+static void parallelForNested(const InputRange *inputRanges, size_t depth,
+                              size_t numThreads, size_t numLoops, Dim *prevDim,
+                              ParallelForFptr func, void *ctx);
 
 template <unsigned N, bool Term, size_t... Is>
-static void run_parallel_for(const InputRange *input_ranges, size_t depth,
-                             size_t num_threads, size_t num_loops,
-                             Dim *prev_dim, parallel_for_fptr func, void *ctx) {
+static void runParallelFor(const InputRange *inputRanges, size_t depth,
+                           size_t numThreads, size_t numLoops, Dim *prevDim,
+                           ParallelForFptr func, void *ctx) {
   std::array<InputRange, N> tempRanges;
-  std::copy_n(input_ranges + depth, N, tempRanges.begin());
+  std::copy_n(inputRanges + depth, N, tempRanges.begin());
 
   if (DEBUG) {
-    std::lock_guard<std::mutex> lock(getDebugMutext());
+    std::lock_guard<std::mutex> lock(getDebugMutex());
     fprintf(stderr, "parallel_for_nested: depth=%d", static_cast<int>(depth));
     for (unsigned i = 0; i < N; ++i) {
       auto &input = tempRanges[i];
-      auto lower_bound = input.lower;
-      auto upper_bound = input.upper;
+      auto lowerBound = input.lower;
+      auto upperBound = input.upper;
       auto step = input.step;
       fprintf(stderr, " (lower_bound=%d, upper_bound=%d, step=%d)",
-              static_cast<int>(lower_bound), static_cast<int>(upper_bound),
+              static_cast<int>(lowerBound), static_cast<int>(upperBound),
               static_cast<int>(step));
     }
     fprintf(stderr, "\n");
@@ -112,56 +131,56 @@ static void run_parallel_for(const InputRange *input_ranges, size_t depth,
 
   auto getRange = [&](size_t i) {
     auto &input = tempRanges[i];
-    auto lower_bound = input.lower;
-    auto upper_bound = input.upper;
+    auto lowerBound = input.lower;
+    auto upperBound = input.upper;
     auto step = input.step;
-    size_t count = (upper_bound - lower_bound + step - 1) / step;
+    size_t count = (upperBound - lowerBound + step - 1) / step;
     size_t grain =
-        std::max(size_t(1), std::min(count / num_threads / 2, size_t(64)));
+        std::max(size_t(1), std::min(count / numThreads / 2, size_t(64)));
     return tbb::blocked_range<size_t>(0, count, grain);
   };
 
   tbb::blocked_rangeNd<size_t, N> range(getRange(Is)...);
 
   auto runFunc = [&](Dim *current) {
-    auto thread_index =
+    auto threadIndex =
         static_cast<size_t>(tbb::this_task_arena::current_thread_index());
-    assert(thread_index >= 0);
-    std::array<Range, 8> static_ranges;
-    std::unique_ptr<Range[]> dyn_ranges;
-    auto *range_ptr = [&]() -> Range * {
-      if (num_loops <= static_ranges.size())
-        return static_ranges.data();
+    assert(threadIndex >= 0);
+    std::array<Range, 8> staticRanges;
+    std::unique_ptr<Range[]> dynRanges;
+    auto *rangePtr = [&]() -> Range * {
+      if (numLoops <= staticRanges.size())
+        return staticRanges.data();
 
-      dyn_ranges.reset(new Range[num_loops]);
-      return dyn_ranges.get();
+      dynRanges.reset(new Range[numLoops]);
+      return dynRanges.get();
     }();
 
-    for (size_t i = 0; i < num_loops; ++i) {
+    for (size_t i = 0; i < numLoops; ++i) {
       assert(current);
-      range_ptr[num_loops - i - 1] = current->val;
+      rangePtr[numLoops - i - 1] = current->val;
       current = current->prev;
     }
 
     if (DEBUG) {
-      std::lock_guard<std::mutex> lock(getDebugMutext());
+      std::lock_guard<std::mutex> lock(getDebugMutex());
       fprintf(stderr, "parallel_for func: thread_index=%d",
-              static_cast<int>(thread_index));
-      for (size_t i = 0; i < num_loops; ++i) {
-        auto &input = range_ptr[i];
-        auto lower_bound = input.lower;
-        auto upper_bound = input.upper;
+              static_cast<int>(threadIndex));
+      for (size_t i = 0; i < numLoops; ++i) {
+        auto &input = rangePtr[i];
+        auto lowerBound = input.lower;
+        auto upperBound = input.upper;
         fprintf(stderr, " (lower_bound=%d, upper_bound=%d)",
-                static_cast<int>(lower_bound), static_cast<int>(upper_bound));
+                static_cast<int>(lowerBound), static_cast<int>(upperBound));
       }
       fprintf(stderr, "\n");
     }
-    func(range_ptr, thread_index, ctx);
+    func(rangePtr, threadIndex, ctx);
   };
 
   auto loopBody = [&](const tbb::blocked_rangeNd<size_t, N> &r) {
     std::array<Dim, N> dims;
-    auto prev = prev_dim;
+    auto prev = prevDim;
     for (unsigned i = 0; i < N; ++i) {
       auto &input = tempRanges[i];
       auto lower_bound = input.lower;
@@ -177,48 +196,46 @@ static void run_parallel_for(const InputRange *input_ranges, size_t depth,
       runFunc(prev);
     } else {
       auto next = depth + N;
-      parallel_for_nested(input_ranges, next, num_threads, num_loops, prev,
-                          func, ctx);
+      parallelForNested(inputRanges, next, numThreads, numLoops, prev, func,
+                        ctx);
     }
   };
 
   tbb::parallel_for(range, loopBody, tbb::auto_partitioner());
 }
 
-static void parallel_for_nested(const InputRange *input_ranges, size_t depth,
-                                size_t num_threads, size_t num_loops,
-                                Dim *prev_dim, parallel_for_fptr func,
-                                void *ctx) {
-  assert(num_loops > depth);
-  auto rem = num_loops - depth;
+static void parallelForNested(const InputRange *inputRanges, size_t depth,
+                              size_t numThreads, size_t numLoops, Dim *prevDim,
+                              ParallelForFptr func, void *ctx) {
+  assert(numLoops > depth);
+  auto rem = numLoops - depth;
   if (rem == 1) {
-    run_parallel_for<1, true, 0>(input_ranges, depth, num_threads, num_loops,
-                                 prev_dim, func, ctx);
+    runParallelFor<1, true, 0>(inputRanges, depth, numThreads, numLoops,
+                               prevDim, func, ctx);
   } else if (rem == 2) {
-    run_parallel_for<2, true, 0, 1>(input_ranges, depth, num_threads, num_loops,
-                                    prev_dim, func, ctx);
+    runParallelFor<2, true, 0, 1>(inputRanges, depth, numThreads, numLoops,
+                                  prevDim, func, ctx);
   } else if (rem == 3) {
-    run_parallel_for<3, true, 0, 1, 2>(input_ranges, depth, num_threads,
-                                       num_loops, prev_dim, func, ctx);
+    runParallelFor<3, true, 0, 1, 2>(inputRanges, depth, numThreads, numLoops,
+                                     prevDim, func, ctx);
   } else {
-    run_parallel_for<3, false, 0, 1, 2>(input_ranges, depth, num_threads,
-                                        num_loops, prev_dim, func, ctx);
+    runParallelFor<3, false, 0, 1, 2>(inputRanges, depth, numThreads, numLoops,
+                                      prevDim, func, ctx);
   }
 }
 } // namespace
 
 extern "C" {
-DPCOMP_RUNTIME_EXPORT void dpcompParallelFor(const InputRange *input_ranges,
-                                             size_t num_loops,
-                                             parallel_for_fptr func,
-                                             void *ctx) {
+DPCOMP_RUNTIME_EXPORT void dpcompParallelFor(const InputRange *inputRanges,
+                                             size_t numLoops,
+                                             ParallelForFptr func, void *ctx) {
   auto &context = getContext();
-  auto num_threads = static_cast<size_t>(context.numThreads);
+  auto numThreads = static_cast<size_t>(context.numThreads);
   if (DEBUG) {
-    std::lock_guard<std::mutex> lock(getDebugMutext());
-    fprintf(stderr, "parallel_for num_loops=%d: ", static_cast<int>(num_loops));
-    for (size_t i = 0; i < num_loops; ++i) {
-      auto r = input_ranges[i];
+    std::lock_guard<std::mutex> lock(getDebugMutex());
+    fprintf(stderr, "parallel_for num_loops=%d: ", static_cast<int>(numLoops));
+    for (size_t i = 0; i < numLoops; ++i) {
+      auto r = inputRanges[i];
       fprintf(stderr, "(%d, %d, %d) ", static_cast<int>(r.lower),
               static_cast<int>(r.upper), static_cast<int>(r.step));
     }
@@ -226,8 +243,7 @@ DPCOMP_RUNTIME_EXPORT void dpcompParallelFor(const InputRange *input_ranges,
   }
 
   context.arena.execute([&] {
-    parallel_for_nested(input_ranges, 0, num_threads, num_loops, nullptr, func,
-                        ctx);
+    parallelForNested(inputRanges, 0, numThreads, numLoops, nullptr, func, ctx);
   });
 }
 
