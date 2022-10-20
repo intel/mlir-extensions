92d6ade13769feb8869f48fb3796a22ded5b254f
Ivan Butygin <ivan.butygin@gmail.com>
2022-10-20 15:18:48 +0200

    fix

--- mlir/lib/Transforms/Cse.cpp
+++ mlir/lib/Transforms/Cse.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "imex/Transforms/cse.hpp"
+#include "imex/Transforms/Cse.hpp"
 
 #include <llvm/ADT/ScopedHashTable.h>
 #include <llvm/Support/Allocator.h>
