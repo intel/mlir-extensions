53b55ad615e48d2f494926ec7986693984bf8118
Ivan Butygin <ivan.butygin@gmail.com>
2022-10-26 02:03:04 +0200

    typo

--- mlir/include/imex/Conversion/NtensorToLinalg.hpp
+++ mlir/include/imex/Conversion/NtensorToLinalg.hpp
@@ -26,7 +26,7 @@ namespace imex {
 void populateNtensorToLinalgPatterns(mlir::MLIRContext &context,
                                      mlir::RewritePatternSet &patterns);
 
-/// Creates a pass for ntensor alias analysis, reauired by ntensor-to-linalg.
+/// Creates a pass for ntensor alias analysis, required by ntensor-to-linalg.
 std::unique_ptr<mlir::Pass> createNtensorAliasAnalysisPass();
 
 /// Creates a pass to convert ntensor array ops to linalg.
