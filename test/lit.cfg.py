d51ebd4cb2a36aa69bff3b563fabfa5c6e4ba546
Sang Ik Lee <sang.ik.lee@intel.com>
2022-11-03 14:43:02 -0700

    Merge upstream refactor and change the way new tests are disabled.

--- test/lit.cfg.py
+++ test/lit.cfg.py
@@ -48,7 +48,7 @@ llvm_config.use_default_substitutions()
 # excludes: A list of directories to exclude from the testsuite. The 'Inputs'
 # subdirectories contain auxiliary inputs for various tests in their parent
 # directories.
-config.excludes = ['Inputs', 'Examples', 'Jax', 'Models', 'CMakeLists.txt', 'README.txt', 'LICENSE.txt']
+config.excludes = ['Inputs', 'Examples', 'CMakeLists.txt', 'README.txt', 'LICENSE.txt']
 
 # test_exec_root: The root path where tests should be run.
 config.test_exec_root = os.path.join(config.imex_obj_root, 'test')
