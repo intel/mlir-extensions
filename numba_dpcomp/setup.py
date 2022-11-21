ef617cc804574612bf276a5d015677f9140df703
Ivan Butygin <ivan.butygin@gmail.com>
2022-11-21 01:33:18 +0100

    fix

--- numba_dpcomp/setup.py
+++ numba_dpcomp/setup.py
@@ -34,7 +34,7 @@ if int(os.environ.get("DPCOMP_SETUP_RUN_CMAKE", 1)):
     TBB_DIR = os.path.join(os.environ["TBB_PATH"], "lib", "cmake", "tbb")
     CMAKE_INSTALL_PREFIX = os.path.join(root_dir, "..")
 
-    cmake_build_dir = os.path.join(root_dir, "cmake_build")
+    cmake_build_dir = os.path.join(CMAKE_INSTALL_PREFIX, "dpcomp_cmake_build")
     cmake_cmd = [
         "cmake",
         root_dir,
