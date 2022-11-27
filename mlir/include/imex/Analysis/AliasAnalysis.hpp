b09a3c637bf75d783580f5550c894b46d5a6efaa
Ivan Butygin <ivan.butygin@gmail.com>
2022-11-27 01:34:23 +0100

    Add alias analysis

---
+++ mlir/include/imex/Analysis/AliasAnalysis.hpp
@@ -0,0 +1,26 @@
+// SPDX-FileCopyrightText: 2022 Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#pragma once
+
+#include <mlir/Analysis/AliasAnalysis.h>
+
+// TODO: this is direct copypaste from llvm LocalAliasAnalysis, upstream.
+// The only difference is that function arguments respects Restrict attr.
+
+namespace imex {
+/// This class implements a local form of alias analysis that tries to identify
+/// the underlying values addressed by each value and performs a few basic
+/// checks to see if they alias.
+class LocalAliasAnalysis {
+public:
+  /// Given two values, return their aliasing behavior.
+  mlir::AliasResult alias(mlir::Value lhs, mlir::Value rhs);
+
+  /// Return the modify-reference behavior of `op` on `location`.
+  mlir::ModRefResult getModRef(mlir::Operation *op, mlir::Value location);
+};
+
+mlir::StringRef getRestrictArgName();
+} // namespace imex
