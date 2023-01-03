b793713522ebbe67ed96b282bec686ae1beed9d7
Ivan Butygin <ivan.butygin@gmail.com>
2023-01-03 13:47:58 +0100

    remove old code

--- mlir/include/imex/Transforms/CanonicalizeReductions.hpp
+++ mlir/include/imex/Transforms/CanonicalizeReductions.hpp
@@ -5,24 +5,12 @@
 #pragma once
 
 #include <memory>
-#include <mlir/IR/PatternMatch.h>
 
 namespace mlir {
 class Pass;
-namespace scf {
-class ForOp;
 }
-} // namespace mlir
 
 namespace imex {
-struct CanonicalizeReduction : public mlir::OpRewritePattern<mlir::scf::ForOp> {
-  using mlir::OpRewritePattern<mlir::scf::ForOp>::OpRewritePattern;
-
-  mlir::LogicalResult
-  matchAndRewrite(mlir::scf::ForOp op,
-                  mlir::PatternRewriter &rewriter) const override;
-};
-
 /// Tries to promote loads/stores in scf.for to loop-carried variables.
 std::unique_ptr<mlir::Pass> createCanonicalizeReductionsPass();
 } // namespace imex
