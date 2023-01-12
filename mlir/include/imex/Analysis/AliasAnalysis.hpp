1ddf8a751d3999977bcc2824d6ea8a8045e47e97
Ivan Butygin <ivan.butygin@gmail.com>
2023-01-12 23:51:39 +0100

    Remove LocalAliasAnalysis copypaste

--- mlir/include/imex/Analysis/AliasAnalysis.hpp
+++ mlir/include/imex/Analysis/AliasAnalysis.hpp
@@ -4,10 +4,7 @@
 
 #pragma once
 
-#include <mlir/Analysis/AliasAnalysis.h>
-
-// TODO: this is direct copypaste from llvm LocalAliasAnalysis, upstream.
-// The only difference is that function arguments respects Restrict attr.
+#include <mlir/Analysis/AliasAnalysis/LocalAliasAnalysis.h>
 
 namespace mlir {
 class Operation;
@@ -17,18 +14,15 @@ namespace imex {
 /// This class implements a local form of alias analysis that tries to identify
 /// the underlying values addressed by each value and performs a few basic
 /// checks to see if they alias.
-class LocalAliasAnalysis {
+class LocalAliasAnalysis : public mlir::LocalAliasAnalysis {
 public:
   LocalAliasAnalysis() = default;
 
   /// Ctor compatible with mlir analysis infra.
   LocalAliasAnalysis(mlir::Operation *){};
 
-  /// Given two values, return their aliasing behavior.
-  mlir::AliasResult alias(mlir::Value lhs, mlir::Value rhs);
-
-  /// Return the modify-reference behavior of `op` on `location`.
-  mlir::ModRefResult getModRef(mlir::Operation *op, mlir::Value location);
+protected:
+  mlir::AliasResult aliasImpl(mlir::Value lhs, mlir::Value rhs) override;
 };
 
 mlir::StringRef getRestrictArgName();
