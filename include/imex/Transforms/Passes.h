55f55a5c269cd4c557593c1b21e5f6191cfc800d
Tuomas Karna <tuomas.karna@intel.com>
2024-04-25 23:51:08 +0300

    Add insert-gpu-copy pass

--- include/imex/Transforms/Passes.h
+++ include/imex/Transforms/Passes.h
@@ -25,6 +25,7 @@ namespace imex {
 std::unique_ptr<mlir::Pass> createSerializeSPIRVPass();
 std::unique_ptr<mlir::Pass>
 createInsertGPUAllocsPass(const char *clientAPI = "vulkan");
+std::unique_ptr<mlir::Pass> createInsertGPUCopyPass();
 std::unique_ptr<mlir::Pass> createSetSPIRVCapabilitiesPass();
 std::unique_ptr<mlir::Pass>
 createSetSPIRVAbiAttributePass(const char *clientAPI = "vulkan");
