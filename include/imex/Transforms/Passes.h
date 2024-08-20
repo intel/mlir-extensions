0e7c002c69487c55c78624216704dc7de0ded40f
Chao Chen <chao.chen@intel.com>
2024-08-20 21:08:43 +0000

    Update the PropagateLayoutPass
    - added support for ExtractOp
    - rename it to apply-vnni-transform

--- include/imex/Transforms/Passes.h
+++ include/imex/Transforms/Passes.h
@@ -31,6 +31,7 @@ std::unique_ptr<mlir::Pass> createRemoveTemporariesPass();
 std::unique_ptr<mlir::Pass> createVectorLinearizePass();
 std::unique_ptr<mlir::Pass> createPropagatePackedLayoutPass();
 std::unique_ptr<mlir::Pass> createRemoveSingleElemVectorPass();
+std::unique_ptr<mlir::Pass> createVnniTransformationPass();
 
 #define GEN_PASS_DECL
 #include "imex/Transforms/Passes.h.inc"
