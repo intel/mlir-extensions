483fcd742591c750cd77ac2c320c6339f32bd358
chudur-budur <1117795+chudur-budur@users.noreply.github.com>
2024-09-19 11:19:31 -0400

    Adding more functionalities to xetile-test-gen (#708)
    * Add --sheet_name and --runtime options
    
    * Add --code_version option, rename default folder names, modify README
    
    * Ignore commented lines while reading csv
    
    * Run the tests as the same order specified in the `input_shapes.csv`
    
    * Add --test_dir option for non-default `generated_gemm`
    
    * Add --timeout to abort long running test cases.
    
    * Parameterize --timeout
    
    * Move the array length check inside `if [[ "$VALIDATE" == 0 ]]`.
    
    * Update README and other small fix.

--- scripts/xetile-test-gen/README.md
+++ scripts/xetile-test-gen/README.md
@@ -12,21 +12,37 @@ _Note: wg* and sg* here mean work group and subgroup tile sizes, sgk means step 
 Matrix A is (MxK), B is (KxN), C is (MxN)._
 
 To get reports for different code versions, run `run_tests.sh`, you need to specify 4 parameters:
-1. `--test_csv` - path to the `.csv` file with testcases in the format mentioned above.
-2. `--validate` - 0 or 1, default is 0. Validation tests won't be profiled.
-3. `--gen_default_cases` - 0 or 1, default is 0. Additionally generates hardcoded test cases for a quick check (e.g., 4kx4k, batched 1kx1k).
-4. `--report_dir` - path to the directory where test reports in text format will be stored, default is `GEMM_reports`, directory is created if it doesn't exist.
-5. `--verbose`: 0 or 1, default is 0. If set to 1, outputs a command for each test that was used to run it.
-6. `--llvm_build_dir`: path to the LLVM directory where imex was built.
-Example usage:
-    Generate tests from csv:
-    ```
-    ./run_tests.sh --test_csv=input_shapes.csv --validate=1 --report_dir=GEMM_reports --llvm_build_dir=../../../llvm-project/build
-    ```
-    Generate only hardcoded tests (e.g., 4kx4k, 1kx1k) for a quick check:
-    ```
-    ./run_tests.sh --gen_default_cases=1 --validate=0 --verbose=1 --llvm_build_dir=../../../llvm-project/build
-    ```
+1. `--test_csv`: The path to the `.csv` file with testcases in the format mentioned above.
+2. `--test_dir`: The path to the directory where all the generated test mlir file will reside. Default `generated-gemm`.
+3. `--report_dir`: The path to the directory where test reports in text format will be stored, default is `gemm_reports`, directory is created if it doesn't exist.
+4. `--sheet_name`: The name of the final output xlsx sheet collating all resutls.
+5. `--llvm_build_dir`: The path to the LLVM directory where imex was built.
+6. `--validate`: 0 or 1, the default is 0. Validation tests won't be profiled.
+7. `--runtime`: The name of the runtime, either `l0-runtime` (default) or `sycl-runtime`.
+8. `--code_version`: To separate `baseline` and `prefetch` runs, will run both if not specified.
+9. `--gen_default_cases`: 0 or 1, the default is 0. Additionally generates hardcoded test cases for a quick check (e.g., 4kx4k, batched 1kx1k).
+10. `--verbose`: 0 or 1, the default is 0. If set to 1, outputs a command for each test that was used to run it.
+11. `--timeout`: A timeout can be set for longer tests, uses GNU `timeout`, see `timeout --help`.
+
+### Example usage:
+
+Generate tests from csv:
+
+```bash
+./run_tests.sh --test_csv=input_shapes.csv --validate=1 --report_dir=gemm_reports --llvm_build_dir=../../../llvm-project/build
+```
+
+Generate only hardcoded tests (e.g., 4kx4k, 1kx1k) for a quick check:
+    
+```bash
+./run_tests.sh --gen_default_cases=1 --validate=0 --verbose=1 --llvm_build_dir=../../../llvm-project/build
+```
+
+Specify the final output sheet name, use a `sycl-runtime` and run only `baseline` code:
+
+```bash
+./run_tests.sh --gen_default_cases=1 --validate=0 --verbose=1 --llvm_build_dir=../../../llvm-project/build --sheet_name=result.xlsx --runtime=sycl-runtime --code_version=baseline
+```
 
 It executes the following workflow for various options:
 1. `gen_xetile_from_shapes.py` reads the tests csv file, for each test case generates a corresponding `.mlir` file.
@@ -35,14 +51,14 @@ It executes the following workflow for various options:
 
 Currently, it generates GEMM code for the baseline implementation and the prefetch version.
 
+-------------------
 
----------------
 `xetile_testgen.py` has 4 parameters that should be specified:
 1. `--code_version` - string, currently test generator supports `baseline` and `prefetch`.
 2. `--validate` - 0 or 1, controls CPU validation. When set to 0, no CPU validation will be performed and env variables `IMEX_ENABLE_PROFILING` will be set to report kernel time.
 3. `--print_debug` - 0 or 1, prints some debug info regarding tile shapes and layouts.
 4. `--test_csv` - path to the `.csv` file with testcases in the format mentioned above.
-`--prefetch` can be true or false: whether to generate code with prefetch or not
+
 -------------------
 
 `report_to_excel.py` has 3 parameters:
@@ -51,10 +67,12 @@ Currently, it generates GEMM code for the baseline implementation and the prefet
 3. `--sheet_name` - path to the spreadsheet to be updated (if `--report_name`` is specified), otherwise will be used as the name of the spreadsheet to build.
 
 Example build from scratch based on reports from directory:
-```
+
+```bash
 python3 report_to_excel.py --reports_dir=../mydir
 ```
 Example update existing:
-```
+
+```bash
 python3 report_to_excel.py --reports_name=../new_code_variant.txt --sheet_name=../existing_sheet.xlsx
 ```
--- scripts/xetile-test-gen/run_tests.sh
+++ scripts/xetile-test-gen/run_tests.sh
@@ -1,21 +1,41 @@
 #!/bin/bash
 
-REPORT_DIR=GEMM_reports
-LLVM=""
 TEST_CSV=""
+TEST_DIR=generated-gemm
+REPORT_DIR=gemm-reports
+SHEET_NAME=speedup-report.xlsx
+LLVM=""
 VALIDATE=0
+RT=l0-runtime
+CODE_VERSION=""
 GEN_DEFAULT_CASES=0
 VERBOSE=0
+TIMEOUT=0
 while (( $# >= 1 )); do
     case $1 in
         --test_csv=*)
             TEST_CSV="${1#*=}"
             ;;
-        --validate=*)
-            VALIDATE="${1#*=}"
+        --test_dir=*)
+            TEST_DIR="${1#*=}"
             ;;
         --report_dir=*)
             REPORT_DIR="${1#*=}"
+            ;;
+        --sheet_name=*)
+            SHEET_NAME="${1#*=}"
+            ;;
+        --llvm_build_dir=*)
+            LLVM="${1#*=}"
+            ;;
+        --validate=*)
+            VALIDATE="${1#*=}"
+            ;;
+        --runtime=*)
+            RT="${1#*=}"
+            ;;
+        --code_version=*)
+            CODE_VERSION="${1#*=}"
             ;;
         --gen_default_cases=*)
             GEN_DEFAULT_CASES="${1#*=}"
@@ -23,8 +43,8 @@ while (( $# >= 1 )); do
         --verbose=*)
             VERBOSE="${1#*=}"
             ;;
-        --llvm_build_dir=*)
-            LLVM="${1#*=}"
+        --timeout=*)
+            TIMEOUT="${1#*=}"
             ;;
         *)
             break
@@ -56,45 +76,93 @@ elif [ $VALIDATE = 1 ]; then
     unset IMEX_ENABLE_PROFILING
 fi
 
+if [[ -z "$CODE_VERSION" ]]; then
+    CODE_VERSIONS=(baseline prefetch)
+elif [[ "$CODE_VERSION" == "baseline" ]]; then
+    CODE_VERSIONS=(baseline)
+elif [[ "$CODE_VERSION" == "prefetch" ]]; then
+    CODE_VERSIONS=(prefetch)
+else 
+    echo "Invalid code version, use either 'baseline' or 'prefetch'."
+    exit 255
+fi
 
 export IMEX_ENABLE_LARGE_REG_FILE=1
 
-TEST_DIR="Generated_GEMM"
 mkdir -p $REPORT_DIR
 RESULT_CMD=0
-for CODE_VERSION in baseline prefetch
-do
-    TEST_REPORT=$CODE_VERSION".txt"
+for CODE_VER in ${CODE_VERSIONS[@]}; do
+    TEST_REPORT=$CODE_VER".txt"
     REPORT_PATH=$REPORT_DIR/$TEST_REPORT
     rm -f $REPORT_PATH
-    echo $'\nTesting code version:' $CODE_VERSION
-    python3 xetile_testgen.py --code_version=$CODE_VERSION --validate=$VALIDATE --print_debug=0 --test_csv=$TEST_CSV --default_tests=$GEN_DEFAULT_CASES
-    CUR_TEST_DIR=$TEST_DIR/$CODE_VERSION
+    echo $'\nTesting code version:' $CODE_VER
+    python3 xetile_testgen.py --code_version=$CODE_VER --validate=$VALIDATE --print_debug=0 --test_csv=$TEST_CSV --default_tests=$GEN_DEFAULT_CASES --output_gemm_dir=$TEST_DIR
+    CUR_TEST_DIR=$TEST_DIR/$CODE_VER
+    if [[ -z "$TEST_CSV" ]]; then
+        # if no test $TEST_CSV is given, list whatever in the $CUR_TEST_DIR
+        TEST_NAMES=($(ls $CUR_TEST_DIR -1))
+    else
+        # else, run the test cases as the same order in the $TEST_CSV file
+        if [[ "$VALIDATE" == "0" ]]; then
+            TEST_NAMES=($(cat -n $TEST_CSV | tail -n +2 \
+                | awk -F' ' '{printf "%s,%s\n", $1,$2}' \
+                | grep -v '#' \
+                | awk -F',' '{printf "sg_gemm_%s_%sx%sx%s_wgm%s_wgn%s_sgm%s_sgn%s_sgk%s.mlir ", $2, $3, $4, $5, $7, $8, $9, $10, $11}'))
+            LINE_NOS=($(cat -n $TEST_CSV | tail -n +2 \
+                | awk -F' ' '{printf "%s,%s\n", $1,$2}' \
+                | grep -v '#' \
+                | awk -F',' '{print $1}'))
+        else
+            TEST_NAMES=($(cat -n $TEST_CSV | tail -n +2 \
+                | awk -F' ' '{printf "%s,%s\n", $1,$2}' \
+                | grep -v '#' \
+                | awk -F',' '$2 > 5000 || $3 > 5000 || $4 > 5000 {next}1' \
+                | awk -F',' '{printf "sg_gemm_%s_%sx%sx%s_wgm%s_wgn%s_sgm%s_sgn%s_sgk%s.mlir ", $2, $3, $4, $5, $7, $8, $9, $10, $11}'))
+            LINE_NOS=($(cat -n $TEST_CSV | tail -n +2 \
+                | awk -F' ' '{printf "%s,%s\n", $1,$2}' \
+                | grep -v '#' \
+                | awk -F',' '$3 > 5000 || $4 > 5000 || $5 > 5000 {next}1' \
+                | awk -F',' '{print $1}'))
+        fi
+        if [[ "${#TEST_NAMES[@]}" != "${#LINE_NOS[@]}" ]]; then
+            echo "\${#TEST_NAME[@]} != \${#LINE_NOS[@]}. Aborting."
+            exit 255
+        fi
+    fi
+    
     set -o pipefail
-    for TEST_CASE in $CUR_TEST_DIR/*
-    do
-        if [ -f "$TEST_CASE" ]
-        then
-            echo "Testing $(basename $TEST_CASE)" | tee -a $REPORT_PATH # report parsing uses this line
-            CMD="python3 $LLVM/bin/imex-runner.py --requires=l0-runtime -i $TEST_CASE  \
+    n=0
+    for TEST_NAME in ${TEST_NAMES[@]}; do
+        TEST_CASE=$CUR_TEST_DIR/$TEST_NAME
+        if [ -f "$TEST_CASE" ]; then
+            echo "${LINE_NOS[$n]}. Testing $(basename $TEST_CASE)" | tee -a $REPORT_PATH # report parsing uses this line
+            if [[ "$RT" == "l0-runtime" ]]; then
+                CMD="python3 $LLVM/bin/imex-runner.py --requires=l0-runtime -i $TEST_CASE  \
                 --pass-pipeline-file=../../test/Integration/Dialect/XeTile/xetile-to-func-vc.pp \
                 --runner imex-cpu-runner -e main --entry-point-result=void \
                 --shared-libs=$LLVM/lib/libimex_runner_utils.so,$LLVM/lib/libmlir_runner_utils.so,$LLVM/lib/libmlir_c_runner_utils.so,$LLVM/lib/liblevel-zero-runtime.so"
+            elif [[ "$RT" == "sycl-runtime" ]]; then
+                CMD="python3 $LLVM/bin/imex-runner.py --requires=sycl-runtime -i $TEST_CASE  \
+                --pass-pipeline-file=../../test/Integration/Dialect/XeTile/xetile-to-func-vc.pp \
+                --runner imex-cpu-runner -e main --entry-point-result=void \
+                --shared-libs=$LLVM/lib/libimex_runner_utils.so,$LLVM/lib/libmlir_runner_utils.so,$LLVM/lib/libmlir_c_runner_utils.so,$LLVM/lib/libsycl-runtime.so"
+            fi
             if [ $VERBOSE -eq 1 ]; then
                 echo $CMD | tee -a $REPORT_PATH
             fi
-            $CMD |& tee -a $REPORT_PATH
+            timeout --signal=SIGRTMAX $TIMEOUT $CMD |& tee -a $REPORT_PATH
             tmp_res=$?
             if [ $tmp_res -ne 0 ]; then
                 RESULT_CMD=1
             fi
             echo "" | tee -a $REPORT_PATH # new line
         fi
+        n=$(($n+1))
     done
 done
 
 unset IMEX_ENABLE_LARGE_REG_FILE
 unset IMEX_ENABLE_PROFILING
 
-python3 report_to_excel.py  --reports_dir=$REPORT_DIR
+python3 report_to_excel.py  --reports_dir=$REPORT_DIR --sheet_name=$SHEET_NAME
 exit $RESULT_CMD
