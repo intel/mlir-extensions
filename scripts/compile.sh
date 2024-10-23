4010d8782cdf97788689de4df760993236b4ed91
Alexei Fedotov <alexei.fedotov@gmail.com>
2024-10-23 20:14:34 +0200

    Add compile.sh

---
+++ scripts/compile.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+set -vx
+
+cd $(dirname "$0")/..
+mlir_dir=$(pwd -P)
+
+cmake -S llvm-project/llvm -B build -GNinja  -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DLLVM_ENABLE_PROJECTS=mlir -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_USE_LINKER=gold -DLLVM_ENABLE_ZSTD=OFF -DLLVM_EXTERNAL_PROJECTS="Imex" -DLLVM_EXTERNAL_IMEX_SOURCE_DIR=. "$@"
+
