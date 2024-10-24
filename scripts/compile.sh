f2684a7e2c0b0da9a9643139fc0c983f166983e6
Alexei Fedotov <alexei.fedotov@gmail.com>
2024-10-24 22:42:10 +0200

    Remove clang use

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -5,4 +5,4 @@ set -vx
 
 cd $(dirname "$0")/..
 
-cmake -S . -B build -GNinja  -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_USE_LINKER=gold -DLLVM_ENABLE_ZSTD=OFF "$@"
+cmake -S . -B build -GNinja  -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_USE_LINKER=gold -DLLVM_ENABLE_ZSTD=OFF "$@"
