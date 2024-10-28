34225214264f55684452f093640c160ad18180a1
Alexei Fedotov <alexei.fedotov@gmail.com>
2024-10-28 15:28:36 +0100

    [Innersource] Move the compile line into the script, cleanup (#798)

--- scripts/sysinfo.sh
+++ scripts/sysinfo.sh
@@ -2,4 +2,4 @@
 
 dpkg -l clang cmake gcc gh \
         python3 \
-        opencl-c-headers ocl-icd-opencl-dev intel-level-zero-gpu intel-igc-core intel-opencl-icd
+        opencl-c-headers ocl-icd-opencl-dev intel-level-zero-gpu intel-igc-core intel-opencl-icd || true
