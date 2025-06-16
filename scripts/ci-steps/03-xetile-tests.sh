6a2934c4c7b72a250dd6105a785a4364aa37b4ac
Garra1980 <igor.zamyatin@intel.com>
2025-06-16 16:35:39 +0200

    Add BMG target-2

--- scripts/ci-steps/03-xetile-tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-cd ${BUILD_HOME}
-rm -rf scripts/xetile-test-gen/GEMM_reports scripts/xetile-test-gen/Generated_GEMM
-cd scripts/xetile-test-gen
-pip install pandas argparse openpyxl
-./run_tests.sh --gen_default_cases=1 --validate=1 --verbose=1 --llvm_build_dir=../../build
